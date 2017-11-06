3abda5916942d47ac04dc9c2a16dbab36910aa20
tobiasKaminsky <tobias@kaminsky.me>
2017-11-06 12:58:00 +0100

    token in header

--- src/com/owncloud/android/lib/resources/files/UnlockFileOperation.java
+++ src/com/owncloud/android/lib/resources/files/UnlockFileOperation.java
@@ -26,7 +26,6 @@ import com.owncloud.android.lib.common.operations.RemoteOperationResult;
 import com.owncloud.android.lib.common.utils.Log_OC;
 
 import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.DeleteMethod;
 
 
@@ -65,18 +64,12 @@ public class UnlockFileOperation extends RemoteOperation {
             deleteMethod = new DeleteMethod(client.getBaseUri() + LOCK_FILE_URL + localId);
             deleteMethod.addRequestHeader(OCS_API_HEADER, OCS_API_HEADER_VALUE);
             deleteMethod.addRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-            NameValuePair[] deleteParams = new NameValuePair[1];
-            deleteParams[0] = new NameValuePair("token", token);
-            deleteMethod.setQueryString(deleteParams);
+            deleteMethod.addRequestHeader("token", token);
 
             int status = client.executeMethod(deleteMethod, SYNC_READ_TIMEOUT, SYNC_CONNECTION_TIMEOUT);
 
-            if (status == HttpStatus.SC_OK) {
-                result = new RemoteOperationResult(true, deleteMethod);
-            } else {
-                result = new RemoteOperationResult(false, deleteMethod);
-            }
+            result = new RemoteOperationResult(status == HttpStatus.SC_OK, deleteMethod);
+            
             client.exhaustResponse(deleteMethod.getResponseBodyAsStream());
         } catch (Exception e) {
             result = new RemoteOperationResult(e);
