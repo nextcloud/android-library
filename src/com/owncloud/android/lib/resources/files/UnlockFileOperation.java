272f024c22203952347dc9c0e50802018cdd73d9
tobiasKaminsky <tobias@kaminsky.me>
2017-12-05 13:42:36 +0100

    update

--- src/com/owncloud/android/lib/resources/files/UnlockFileOperation.java
+++ src/com/owncloud/android/lib/resources/files/UnlockFileOperation.java
@@ -67,16 +67,6 @@ public class UnlockFileOperation extends RemoteOperation {
 
             int status = client.executeMethod(deleteMethod, SYNC_READ_TIMEOUT, SYNC_CONNECTION_TIMEOUT);
 
-            if (status == HttpStatus.SC_OK) {
-                result = new RemoteOperationResult(true, deleteMethod);
-            } else {
-                result = new RemoteOperationResult(false, deleteMethod);
-                client.exhaustResponse(deleteMethod.getResponseBodyAsStream());
-            }
-            deleteMethod.addRequestHeader("token", token);
-
-            int status = client.executeMethod(deleteMethod, SYNC_READ_TIMEOUT, SYNC_CONNECTION_TIMEOUT);
-
             result = new RemoteOperationResult(status == HttpStatus.SC_OK, deleteMethod);
             
             client.exhaustResponse(deleteMethod.getResponseBodyAsStream());
