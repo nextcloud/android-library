c8415d72421eda87528f4b98197ade28233e5a7b
tobiasKaminsky <tobias@kaminsky.me>
2019-02-08 17:03:06 +0100

    add support for delete a notification
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

---
+++ src/main/java/com/owncloud/android/lib/resources/notifications/DeleteNotificationRemoteOperation.java
@@ -0,0 +1,105 @@
+/* Nextcloud Android Library is available under MIT license
+ *
+ *   @author Tobias Kaminsky
+ *   Copyright (C) 2019 Tobias Kaminsky
+ *   Copyright (C) 2019 Nextcloud GmbH
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ *   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ *   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ *
+ */
+
+package com.owncloud.android.lib.resources.notifications;
+
+import com.owncloud.android.lib.common.OwnCloudClient;
+import com.owncloud.android.lib.common.operations.RemoteOperation;
+import com.owncloud.android.lib.common.operations.RemoteOperationResult;
+import com.owncloud.android.lib.common.utils.Log_OC;
+import com.owncloud.android.lib.resources.status.OwnCloudVersion;
+
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.jackrabbit.webdav.client.methods.DeleteMethod;
+
+/**
+ * Delete a notification, specified at
+ * {@link "https://github.com/nextcloud/notifications/blob/master/docs/ocs-endpoint-v2.md"}.
+ */
+public class DeleteNotificationRemoteOperation extends RemoteOperation {
+
+    // OCS Route
+    private static final String OCS_ROUTE_LIST_V12_AND_UP =
+            "/ocs/v2.php/apps/notifications/api/v2/notifications/";
+    private static final String OCS_ROUTE_LIST_V9_AND_UP =
+            "/ocs/v2.php/apps/notifications/api/v1/notifications/";
+
+    private static final String TAG = DeleteNotificationRemoteOperation.class.getSimpleName();
+
+    private int id;
+
+    public DeleteNotificationRemoteOperation(int id) {
+        this.id = id;
+    }
+
+    @Override
+    protected RemoteOperationResult run(OwnCloudClient client) {
+        RemoteOperationResult result;
+        int status;
+        DeleteMethod delete = null;
+        String url;
+        if (client.getOwnCloudVersion().compareTo(OwnCloudVersion.nextcloud_12) >= 0) {
+            url = client.getBaseUri() + OCS_ROUTE_LIST_V12_AND_UP;
+        } else {
+            url = client.getBaseUri() + OCS_ROUTE_LIST_V9_AND_UP;
+        }
+
+        try {
+            delete = new DeleteMethod(url + id);
+            delete.addRequestHeader(OCS_API_HEADER, OCS_API_HEADER_VALUE);
+
+            status = client.executeMethod(delete);
+            String response = delete.getResponseBodyAsString();
+
+            if (isSuccess(status)) {
+                result = new RemoteOperationResult(true, status, delete.getResponseHeaders());
+                Log_OC.d(TAG, "Successful response: " + response);
+            } else {
+                result = new RemoteOperationResult(false, status, delete.getResponseHeaders());
+                Log_OC.e(TAG, "Failed response while getting user notifications ");
+                if (response != null) {
+                    Log_OC.e(TAG, "*** status code: " + status + " ; response message: " + response);
+                } else {
+                    Log_OC.e(TAG, "*** status code: " + status);
+                }
+            }
+        } catch (Exception e) {
+            result = new RemoteOperationResult(e);
+            Log_OC.e(TAG, "Exception while getting remote notifications", e);
+        } finally {
+            if (delete != null) {
+                delete.releaseConnection();
+            }
+        }
+
+        return result;
+    }
+
+    private boolean isSuccess(int status) {
+        return (status == HttpStatus.SC_OK);
+    }
+}
