ee4b93bdd9d228190fe2ba15a44f255a3caf873b
tobiasKaminsky <tobias@kaminsky.me>
2019-04-05 09:40:17 +0100

    add support for remote wipe: - check if app token needs to be wiped - notify server on successful wipe - add version check
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

---
+++ src/main/java/com/owncloud/android/lib/resources/users/CheckRemoteWipeRemoteOperation.java
@@ -0,0 +1,99 @@
+/* Nextcloud Android Library is available under MIT license
+ *
+ *   @author Tobias Kaminsky
+ *   Copyright (C) 2019 Tobias Kaminsky
+ *   Copyright (C) 2019 Nextcloud GmbH
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ *   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ *   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ *
+ */
+
+package com.owncloud.android.lib.resources.users;
+
+import com.owncloud.android.lib.common.OwnCloudClient;
+import com.owncloud.android.lib.common.operations.RemoteOperation;
+import com.owncloud.android.lib.common.operations.RemoteOperationResult;
+import com.owncloud.android.lib.common.utils.Log_OC;
+
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.json.JSONObject;
+
+
+/**
+ * Remote operation performing check if app token is scheduled for remote wipe
+ */
+
+public class CheckRemoteWipeRemoteOperation extends RemoteOperation {
+
+    private static final String TAG = CheckRemoteWipeRemoteOperation.class.getSimpleName();
+    private static final int SYNC_READ_TIMEOUT = 40000;
+    private static final int SYNC_CONNECTION_TIMEOUT = 5000;
+    private static final String REMOTE_WIPE_URL = "/index.php/core/wipe/check";
+
+    private static final String TOKEN = "token";
+
+    // JSON node names
+    private static final String JSON_FORMAT = "?format=json";
+    private static final String WIPE = "wipe";
+
+    /**
+     * @param client Client object
+     */
+    @Override
+    protected RemoteOperationResult run(OwnCloudClient client) {
+        PostMethod postMethod = null;
+        RemoteOperationResult result;
+
+        try {
+            postMethod = new PostMethod(client.getBaseUri() + REMOTE_WIPE_URL + JSON_FORMAT);
+            postMethod.addRequestHeader(CONTENT_TYPE, FORM_URLENCODED);
+            postMethod.setParameter(TOKEN, client.getCredentials().getAuthToken());
+
+            int status = client.executeMethod(postMethod, SYNC_READ_TIMEOUT, SYNC_CONNECTION_TIMEOUT);
+
+            if (HttpStatus.SC_OK == status) {
+                String response = postMethod.getResponseBodyAsString();
+
+                JSONObject json = new JSONObject(response);
+
+                if (json.getBoolean(WIPE)) {
+                    result = new RemoteOperationResult(true, postMethod);
+                } else {
+                    result = new RemoteOperationResult(false, postMethod);
+                }
+            } else {
+                result = new RemoteOperationResult(false, postMethod);
+            }
+
+            client.exhaustResponse(postMethod.getResponseBodyAsStream());
+        } catch (Exception e) {
+            result = new RemoteOperationResult(e);
+            Log_OC.e(TAG,
+                     "Getting remote wipe status failed: " + result.getLogMessage(),
+                     result.getException());
+        } finally {
+            if (postMethod != null) {
+                postMethod.releaseConnection();
+            }
+        }
+        return result;
+    }
+}
