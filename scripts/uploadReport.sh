0a6970b9747faa60584a8e83b2156bf2a36fb06f
tobiasKaminsky <tobias@kaminsky.me>
2019-04-17 11:18:49 +0200

    add comment at PR on failing tests
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- scripts/uploadReport.sh
+++ scripts/uploadReport.sh
@@ -16,4 +16,6 @@ find . -type d -exec curl -u $USER:$PASS -X MKCOL $URL/$ID/$(echo {} | sed s#\./
 find . -type f -exec curl -u $USER:$PASS -X PUT $URL/$ID/$(echo {} | sed s#\./##) --upload-file {} \;
 
 echo "Uploaded failing library tests to https://www.kaminsky.me/nc-dev/library-integrationTests/$ID"
+
+curl -u $6:$7 -X POST https://api.github.com/repos/nextcloud/android-library/issues/$5/comments -d "{ \"body\" : \"$TYPE test failed: https://www.kaminsky.me/nc-dev/library-integrationTests/$ID \" }"
 exit 1
