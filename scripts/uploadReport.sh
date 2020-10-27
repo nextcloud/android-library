e12dce2d08717623abec090dfcb4b093b42b27e7
tobiasKaminsky <tobias@kaminsky.me>
2020-10-27 15:01:21 +0100

    fix upload report
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- scripts/uploadReport.sh
+++ scripts/uploadReport.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 URL=https://nextcloud.kaminsky.me/remote.php/webdav/library-integrationTests
-ID=$3
 USER=$1
 PASS=$2
+ID=$3
 BRANCH=$4
 TYPE=$5
+PR_ID=$6
+GIT_USERNAME=$7
+GIT_TOKEN=$8
 REMOTE_FOLDER=$ID-$TYPE-$BRANCH
 
 if [ $TYPE = "IT" ]; then
@@ -19,5 +22,5 @@ find . -type f -exec curl -u $USER:$PASS -X PUT $URL/$REMOTE_FOLDER/$(echo {} | 
 
 echo "Uploaded failing library tests to https://www.kaminsky.me/nc-dev/library-integrationTests/$REMOTE_FOLDER"
 
-curl -u $6:$7 -X POST https://api.github.com/repos/nextcloud/android-library/issues/$5/comments -d "{ \"body\" : \"$TYPE test failed: https://www.kaminsky.me/nc-dev/library-integrationTests/$REMOTE_FOLDER \" }"
+curl -u $GIT_USERNAME:$GIT_TOKEN -X POST https://api.github.com/repos/nextcloud/android-library/issues/$PR_ID/comments -d "{ \"body\" : \"$TYPE test failed: https://www.kaminsky.me/nc-dev/library-integrationTests/$REMOTE_FOLDER \" }"
 exit 1
