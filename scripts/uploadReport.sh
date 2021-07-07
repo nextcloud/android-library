6f1ae6642ee15045d192a6a217fc8bc72c979782
tobiasKaminsky <tobias@kaminsky.me>
2021-07-07 11:03:54 +0200

    delete outdated comments
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- scripts/uploadReport.sh
+++ scripts/uploadReport.sh
@@ -9,7 +9,8 @@ TYPE=$5
 PR_ID=$6
 GIT_USERNAME=$7
 GIT_TOKEN=$8
-REMOTE_FOLDER=$ID-$TYPE-$BRANCH
+REMOTE_FOLDER=$ID-$TYPE-$BRANCH-$(date +%H-%M)
+BRANCH_TYPE=$BRANCH-$TYPE
 
 if [ $TYPE = "IT" ]; then
     cd build/reports/androidTests/connected
@@ -22,5 +23,5 @@ find . -type f -exec curl -u $USER:$PASS -X PUT $URL/$REMOTE_FOLDER/$(echo {} | 
 
 echo "Uploaded failing library tests to https://www.kaminsky.me/nc-dev/library-integrationTests/$REMOTE_FOLDER"
 
-curl -u $GIT_USERNAME:$GIT_TOKEN -X POST https://api.github.com/repos/nextcloud/android-library/issues/$PR_ID/comments -d "{ \"body\" : \"$TYPE test failed: https://www.kaminsky.me/nc-dev/library-integrationTests/$REMOTE_FOLDER \" }"
+curl -u $GIT_USERNAME:$GIT_TOKEN -X POST https://api.github.com/repos/nextcloud/android-library/issues/$PR_ID/comments -d "{ \"body\" : \"$BRANCH_TYPE test failed: https://www.kaminsky.me/nc-dev/library-integrationTests/$REMOTE_FOLDER \" }"
 exit 1
