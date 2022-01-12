4571d11883ce2f4d420c20d3c65fcad7cfc1f864
Álvaro Brey Vilas <alvaro.brey@nextcloud.com>
2022-01-12 10:35:12 +0100

    IPv4FallbackInterceptor: also retry with IPv4 for timeouts
    Signed-off-by: Álvaro Brey Vilas <alvaro.brey@nextcloud.com>

--- src/main/java/com/nextcloud/common/IPv4FallbackInterceptor.kt
+++ src/main/java/com/nextcloud/common/IPv4FallbackInterceptor.kt
@@ -1,25 +1,48 @@
 package com.nextcloud.common
 
+import com.owncloud.android.lib.common.utils.Log_OC
 import okhttp3.ConnectionPool
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
+import java.net.SocketTimeoutException
 
 @Suppress("MagicNumber")
 class IPv4FallbackInterceptor(private val connectionPool: ConnectionPool) : Interceptor {
+    companion object {
+        private const val TAG = "IPv4FallbackInterceptor"
+    }
+
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
         val response = chain.proceed(request)
 
         val hostname = request.url.host
 
-        return if (response.code in 500..599 && IPV6PreferringDNS.isIPV6(hostname)) {
-            IPV6PreferringDNS.setIPVersionPreference(hostname, true)
-            connectionPool.evictAll()
-            chain.proceed(
-                request
-            )
-        } else {
-            response
+        return try {
+            if (response.code in 500..599 && IPV6PreferringDNS.isIPV6(hostname)) {
+                Log_OC.d(TAG, "Response error with IPv6, trying IPv4")
+                retryWithIPv4(hostname, chain, request)
+            } else {
+                response
+            }
+        } catch (e: SocketTimeoutException) {
+            if (IPV6PreferringDNS.isIPV6(hostname)) {
+                Log_OC.d(TAG, "Socket timeout with IPv6, trying IPv4")
+                retryWithIPv4(hostname, chain, request)
+            } else {
+                throw e
+            }
         }
+    }
+
+    private fun retryWithIPv4(
+        hostname: String,
+        chain: Interceptor.Chain,
+        request: Request
+    ): Response {
+        IPV6PreferringDNS.setIPVersionPreference(hostname, true)
+        connectionPool.evictAll()
+        return chain.proceed(request)
     }
 }
