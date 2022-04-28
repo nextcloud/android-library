217a8331a3dab01dfce7e2a15bdf00e4bf188332
tobiasKaminsky <tobias@kaminsky.me>
2022-04-28 07:59:07 +0200

    Test only on NC24+
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- library/src/androidTest/java/com/nextcloud/android/lib/resources/files/ToggleFileLockRemoteOperationIT.kt
+++ library/src/androidTest/java/com/nextcloud/android/lib/resources/files/ToggleFileLockRemoteOperationIT.kt
@@ -32,15 +32,26 @@ import com.owncloud.android.lib.resources.files.ReadFileRemoteOperation
 import com.owncloud.android.lib.resources.files.UploadFileRemoteOperation
 import com.owncloud.android.lib.resources.files.model.FileLockType
 import com.owncloud.android.lib.resources.files.model.RemoteFile
+import com.owncloud.android.lib.resources.status.GetCapabilitiesRemoteOperation
+import com.owncloud.android.lib.resources.status.NextcloudVersion.Companion.nextcloud_24
+import com.owncloud.android.lib.resources.status.OCCapability
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Assume
 import org.junit.Test
 
 class ToggleFileLockRemoteOperationIT : AbstractIT() {
 
     @Test
     fun lockUnlockFile() {
+        // only on >= NC24
+        val ocCapability = GetCapabilitiesRemoteOperation()
+            .execute(nextcloudClient).singleData as OCCapability
+        Assume.assumeTrue(
+            ocCapability.version.isNewerOrEqual(nextcloud_24)
+        )
+
         // create file
         val filePath: String = createFile("text")
         val remotePath = "/text.md"
@@ -48,7 +59,8 @@ class ToggleFileLockRemoteOperationIT : AbstractIT() {
             UploadFileRemoteOperation(filePath, remotePath, "text/markdown", "1464818400")
                 .execute(client).isSuccess
         )
-        val initialFile = ReadFileRemoteOperation(remotePath).execute(client).singleData as RemoteFile
+        val initialFile =
+            ReadFileRemoteOperation(remotePath).execute(client).singleData as RemoteFile
         assertFalse("File shouldn't be locked", initialFile.isLocked)
 
         // lock file
