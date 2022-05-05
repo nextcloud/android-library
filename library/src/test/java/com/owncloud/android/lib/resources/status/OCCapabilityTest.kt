5c4d33f9d99ffe675e7be51e4f5517ed54d0ac7d
Álvaro Brey <alvaro.brey@nextcloud.com>
2022-05-05 16:05:38 +0200

    OCCapability: test nullability of some fields
    Signed-off-by: Álvaro Brey <alvaro.brey@nextcloud.com>

---
+++ library/src/test/java/com/owncloud/android/lib/resources/status/OCCapabilityTest.kt
@@ -0,0 +1,53 @@
+/*
+ * Nextcloud Android Library is available under MIT license
+ *
+ * @author Álvaro Brey Vilas
+ * Copyright (C) 2022 Álvaro Brey Vilas
+ * Copyright (C) 2022 Nextcloud GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.owncloud.android.lib.resources.status
+
+import org.junit.Test
+
+class OCCapabilityTest {
+
+    /**
+     * This won't compile if the fields below are not nullable. This is sort of redundant,
+     * but it's meant to prevent a crash in client apps when trying to assign null from Java.
+     */
+    @Test
+    fun testFieldNullability() {
+        OCCapability().apply {
+            serverName = null
+            serverSlogan = null
+            serverColor = null
+            serverTextColor = null
+            serverElementColor = null
+            serverElementColorBright = null
+            serverElementColorDark = null
+            serverLogo = null
+            serverBackground = null
+            directEditingEtag = null
+        }
+    }
+}
