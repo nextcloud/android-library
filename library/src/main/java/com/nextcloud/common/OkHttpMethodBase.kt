f5762f4dc35b74a76a0b887744027fd5b59d5fe4
Álvaro Brey <alvaro.brv@gmail.com>
2022-07-04 15:25:47 +0200

    Revert "Bump kotlin_version from 1.6.21 to 1.7.0"

--- library/src/main/java/com/nextcloud/common/OkHttpMethodBase.kt
+++ library/src/main/java/com/nextcloud/common/OkHttpMethodBase.kt
@@ -60,12 +60,9 @@ abstract class OkHttpMethodBase(
     }
 
     fun buildQueryParameter(): HttpUrl {
-        val httpBuilder =
-            uri.toHttpUrlOrNull()?.newBuilder() ?: throw IllegalStateException("Error")
+        val httpBuilder = uri.toHttpUrlOrNull()?.newBuilder() ?: throw IllegalStateException("Error")
 
-        for ((k, v) in queryMap) {
-            httpBuilder.addQueryParameter(k, v)
-        }
+        queryMap.forEach { (k, v) -> httpBuilder.addQueryParameter(k, v) }
 
         return httpBuilder.build()
     }
@@ -137,9 +134,7 @@ abstract class OkHttpMethodBase(
 
         requestHeaders[AUTHORIZATION] = nextcloudClient.credentials
         requestHeaders[USER_AGENT] = OwnCloudClientManagerFactory.getUserAgent()
-        for ((name, value) in requestHeaders) {
-            temp.header(name, value)
-        }
+        requestHeaders.forEach { (name, value) -> temp.header(name, value) }
 
         if (useOcsApiRequestHeader) {
             temp.header(RemoteOperation.OCS_API_HEADER, RemoteOperation.OCS_API_HEADER_VALUE)
@@ -166,9 +161,7 @@ abstract class OkHttpMethodBase(
         val temp = requestBuilder.url(buildQueryParameter())
 
         requestHeaders[USER_AGENT] = OwnCloudClientManagerFactory.getUserAgent()
-        for ((name, value) in requestHeaders) {
-            temp.header(name, value)
-        }
+        requestHeaders.forEach { (name, value) -> temp.header(name, value) }
 
         applyType(temp)
 
