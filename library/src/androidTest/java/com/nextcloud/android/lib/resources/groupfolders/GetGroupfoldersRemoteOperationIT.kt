a69fab65b6274c302dd4656fb942aa14bf0e421c
tobiasKaminsky <tobias@kaminsky.me>
2023-05-03 08:46:33 +0200

    test depend on capability
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- library/src/androidTest/java/com/nextcloud/android/lib/resources/groupfolders/GetGroupfoldersRemoteOperationIT.kt
+++ library/src/androidTest/java/com/nextcloud/android/lib/resources/groupfolders/GetGroupfoldersRemoteOperationIT.kt
@@ -28,12 +28,19 @@
 package com.nextcloud.android.lib.resources.groupfolders
 
 import com.owncloud.android.AbstractIT
+import com.owncloud.android.lib.resources.status.GetCapabilitiesRemoteOperation
+import com.owncloud.android.lib.resources.status.OCCapability
 import org.junit.Assert.assertEquals
+import org.junit.Assume.assumeTrue
 import org.junit.Test
 
 class GetGroupfoldersRemoteOperationIT : AbstractIT() {
     @Test
     fun getGroupfolders() {
+        val capability = GetCapabilitiesRemoteOperation().execute(client).resultData as OCCapability
+
+        assumeTrue(capability.groupfolders.isTrue)
+
         val map = GetGroupfoldersRemoteOperation().execute(nextcloudClient).resultData
         assertEquals(1, map.size)
         assertEquals("groupfolder", map["1"]?.mountPoint)
