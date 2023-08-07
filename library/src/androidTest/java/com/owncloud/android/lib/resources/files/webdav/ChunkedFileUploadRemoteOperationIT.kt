9f5b06dedc9da879bf924b01e796940aadb8e069
ZetaTom <70907959+ZetaTom@users.noreply.github.com>
2023-08-07 16:17:53 +0200

    Add test case for resuming chunked uploads
    Signed-off-by: ZetaTom <70907959+ZetaTom@users.noreply.github.com>

--- library/src/androidTest/java/com/owncloud/android/lib/resources/files/webdav/ChunkedFileUploadRemoteOperationIT.kt
+++ library/src/androidTest/java/com/owncloud/android/lib/resources/files/webdav/ChunkedFileUploadRemoteOperationIT.kt
@@ -22,13 +22,17 @@
 package com.owncloud.android.lib.resources.files.webdav
 
 import com.owncloud.android.AbstractIT
+import com.owncloud.android.lib.common.network.WebdavEntry
+import com.owncloud.android.lib.common.network.WebdavUtils
 import com.owncloud.android.lib.common.operations.RemoteOperationResult
 import com.owncloud.android.lib.common.operations.RemoteOperationResult.ResultCode
 import com.owncloud.android.lib.resources.files.ChunkedFileUploadRemoteOperation
 import junit.framework.TestCase
 import junit.framework.TestCase.assertNotNull
 import junit.framework.TestCase.assertTrue
+import org.apache.jackrabbit.webdav.client.methods.PropFindMethod
 import org.junit.Test
+import java.io.File
 
 class ChunkedFileUploadRemoteOperationIT : AbstractIT() {
     @Test
@@ -70,6 +74,65 @@ class ChunkedFileUploadRemoteOperationIT : AbstractIT() {
         TestCase.assertSame(ResultCode.CANCELLED, uploadResult?.code)
     }
 
+    @Test
+    fun resume() {
+        val filePath = createFile("chunkedFile.txt", BIG_FILE_ITERATION * 2)
+        val timestamp = System.currentTimeMillis() / MILLI_TO_SECOND
+        val remotePath = "/bigFile.md"
+
+        // set up first upload
+        var sut = ChunkedFileUploadRemoteOperation(
+            filePath,
+            remotePath,
+            "text/markdown",
+            "",
+            RANDOM_MTIME,
+            timestamp,
+            false,
+            true
+        )
+
+        // start first upload
+        var uploadResult: RemoteOperationResult<String>? = null
+        Thread {
+            uploadResult = sut.execute(client)
+        }.start()
+
+        // delay and cancel upload
+        shortSleep()
+        shortSleep()
+        sut.cancel(ResultCode.CANCELLED)
+
+        for (i in 1..MAX_TRIES) {
+            shortSleep()
+
+            if (uploadResult != null) {
+                break
+            }
+        }
+
+        // start second upload of same file
+        sut = ChunkedFileUploadRemoteOperation(
+            filePath,
+            remotePath,
+            "text/markdown",
+            "",
+            RANDOM_MTIME,
+            timestamp,
+            false,
+            true
+        )
+
+        // reset result; start second upload
+        uploadResult = null
+        uploadResult = sut.execute(client)
+
+        // second upload should succeed
+        assert(uploadResult?.isSuccess == true)
+
+        assert(File(filePath).length() == getRemoteSize(remotePath))
+    }
+
     private fun genLargeUpload(onWifiConnection: Boolean): ChunkedFileUploadRemoteOperation {
         // create file
         val filePath = createFile("chunkedFile.txt", BIG_FILE_ITERATION)
@@ -85,6 +148,15 @@ class ChunkedFileUploadRemoteOperationIT : AbstractIT() {
             onWifiConnection,
             true
         )
+    }
+
+    private fun getRemoteSize(remotePath: String): Long {
+        val davPath = client.filesDavUri.toString() + "/" + WebdavUtils.encodePath(remotePath)
+        val propFindMethod = PropFindMethod(davPath, WebdavUtils.getFilePropSet(), 0)
+        client.executeMethod(propFindMethod)
+        assert(propFindMethod.succeeded())
+
+        return WebdavEntry(propFindMethod.responseBodyAsMultiStatus.responses[0], remotePath).contentLength
     }
 
     companion object {
