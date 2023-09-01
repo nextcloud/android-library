48b06dc635756d4e4df1430cbfd6b9edde915251
tobiasKaminsky <tobias@kaminsky.me>
2023-09-01 09:44:42 +0200

    Remote operation part2
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- library/src/main/java/com/owncloud/android/lib/common/utils/WebDavFileUtils.java
+++ library/src/main/java/com/owncloud/android/lib/common/utils/WebDavFileUtils.java
@@ -17,6 +17,7 @@ import com.owncloud.android.lib.common.network.WebdavEntry;
 import com.owncloud.android.lib.resources.files.model.RemoteFile;
 import com.owncloud.android.lib.resources.files.webdav.NCEtag;
 import com.owncloud.android.lib.resources.files.webdav.NCFavorite;
+import com.owncloud.android.lib.resources.files.webdav.NCGetLastModified;
 import com.owncloud.android.lib.resources.files.webdav.NCMountType;
 import com.owncloud.android.lib.resources.files.webdav.NCPermissions;
 import com.owncloud.android.lib.resources.files.webdav.NCRichWorkspace;
@@ -31,7 +32,6 @@ import java.util.List;
 import at.bitfire.dav4jvm.Property;
 import at.bitfire.dav4jvm.Response;
 import at.bitfire.dav4jvm.property.GetContentType;
-import at.bitfire.dav4jvm.property.GetLastModified;
 import at.bitfire.dav4jvm.property.ResourceType;
 import okhttp3.MediaType;
 
@@ -101,8 +101,8 @@ public class WebDavFileUtils {
                 remoteFile.setFavorite(((NCFavorite) property).isOcFavorite());
             }
 
-            if (property instanceof GetLastModified) {
-                remoteFile.setModifiedTimestamp(((GetLastModified) property).getLastModified());
+            if (property instanceof NCGetLastModified) {
+                remoteFile.setModifiedTimestamp(((NCGetLastModified) property).getLastModified());
             }
 
             if (property instanceof GetContentType) {
---
+++ library/src/main/java/com/owncloud/android/lib/resources/files/webdav/NCGetLastModified.kt
@@ -0,0 +1,55 @@
+/*
+ *
+ * Nextcloud Android client application
+ *
+ * @author Tobias Kaminsky
+ * Copyright (C) 2023 Tobias Kaminsky
+ * Copyright (C) 2023 Nextcloud GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.owncloud.android.lib.resources.files.webdav
+
+import at.bitfire.dav4jvm.Property
+import at.bitfire.dav4jvm.PropertyFactory
+import at.bitfire.dav4jvm.XmlUtils
+import com.owncloud.android.lib.common.network.WebdavUtils
+import org.xmlpull.v1.XmlPullParser
+
+class NCGetLastModified internal constructor(var lastModified: Long) : Property {
+
+    class Factory : PropertyFactory {
+
+        override fun getName() = NAME
+
+        override fun create(parser: XmlPullParser): NCGetLastModified? {
+            // <!ELEMENT getlastmodified (#PCDATA) >
+            XmlUtils.readText(parser)?.let { rawDate ->
+                val date = WebdavUtils.parseResponseDate(rawDate)
+                if (date != null) {
+                    return NCGetLastModified(date.time)
+                } else {
+                    0
+                }
+            }
+            return null
+        }
+    }
+
+    companion object {
+        @JvmField
+        val NAME = Property.Name(XmlUtils.NS_WEBDAV, "getlastmodified")
+    }
+}
