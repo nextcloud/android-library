63985b61d1f88d4c723a3700b3bc9a8932bd4ed6
ZetaTom <70907959+ZetaTom@users.noreply.github.com>
2023-11-27 14:08:32 +0100

    Deprecate OwncloudClient - DirectEditing
    Signed-off-by: ZetaTom <70907959+ZetaTom@users.noreply.github.com>

--- library/src/main/java/com/nextcloud/android/lib/resources/directediting/DirectEditingObtainListOfTemplatesRemoteOperation.java
+++ library/src/main/java/com/nextcloud/android/lib/resources/directediting/DirectEditingObtainListOfTemplatesRemoteOperation.java
@@ -28,7 +28,8 @@
 package com.nextcloud.android.lib.resources.directediting;
 
 import com.google.gson.reflect.TypeToken;
-import com.owncloud.android.lib.common.OwnCloudClient;
+import com.nextcloud.common.NextcloudClient;
+import com.nextcloud.operations.GetMethod;
 import com.owncloud.android.lib.common.TemplateList;
 import com.owncloud.android.lib.common.operations.RemoteOperationResult;
 import com.owncloud.android.lib.common.utils.Log_OC;
@@ -36,7 +37,6 @@ import com.owncloud.android.lib.ocs.ServerResponse;
 import com.owncloud.android.lib.resources.OCSRemoteOperation;
 
 import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.methods.GetMethod;
 
 /**
  * Get templates for an editor
@@ -44,8 +44,6 @@ import org.apache.commons.httpclient.methods.GetMethod;
 
 public class DirectEditingObtainListOfTemplatesRemoteOperation extends OCSRemoteOperation<TemplateList> {
     private static final String TAG = DirectEditingObtainListOfTemplatesRemoteOperation.class.getSimpleName();
-    private static final int SYNC_READ_TIMEOUT = 40000;
-    private static final int SYNC_CONNECTION_TIMEOUT = 5000;
     private static final String DIRECT_ENDPOINT = "/ocs/v2.php/apps/files/api/v1/directEditing/templates/";
 
     private final String editor;
@@ -56,37 +54,34 @@ public class DirectEditingObtainListOfTemplatesRemoteOperation extends OCSRemote
         this.template = template;
     }
 
-    protected RemoteOperationResult<TemplateList> run(OwnCloudClient client) {
+    public RemoteOperationResult<TemplateList> run(NextcloudClient client) {
         RemoteOperationResult<TemplateList> result;
-        GetMethod getMethod = null;
+        com.nextcloud.operations.GetMethod get = null;
 
         try {
-            getMethod = new GetMethod(client.getBaseUri() + DIRECT_ENDPOINT + editor + "/" + template + JSON_FORMAT);
+            // get request
+            get = new GetMethod(client.getBaseUri() + DIRECT_ENDPOINT + editor + "/" + template + JSON_FORMAT, true);
 
-            // remote request
-            getMethod.addRequestHeader(OCS_API_HEADER, OCS_API_HEADER_VALUE);
-
-            int status = client.executeMethod(getMethod, SYNC_READ_TIMEOUT, SYNC_CONNECTION_TIMEOUT);
+            int status = client.execute(get);
 
             if (status == HttpStatus.SC_OK) {
-                TemplateList templateList = getServerResponse(getMethod,
+                TemplateList templateList = getServerResponse(get,
                                                               new TypeToken<ServerResponse<TemplateList>>() {
                                                               })
                         .getOcs().getData();
 
-                result = new RemoteOperationResult<>(true, getMethod);
+                result = new RemoteOperationResult<>(true, get);
                 result.setResultData(templateList);
             } else {
-                result = new RemoteOperationResult<>(false, getMethod);
-                client.exhaustResponse(getMethod.getResponseBodyAsStream());
+                result = new RemoteOperationResult<>(false, get);
             }
         } catch (Exception e) {
             result = new RemoteOperationResult<>(e);
             Log_OC.e(TAG, "Get all direct editing information failed: " + result.getLogMessage(),
                      result.getException());
         } finally {
-            if (getMethod != null) {
-                getMethod.releaseConnection();
+            if (get != null) {
+                get.releaseConnection();
             }
         }
         return result;
