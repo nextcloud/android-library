bceb2167cb50c7b8495a65936aad171fcfbb38e3
alperozturk <alper_ozturk@proton.me>
2024-02-28 15:01:43 +0100

    Add GetTaskListRemoteOperation
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

---
+++ library/src/main/java/com/owncloud/android/lib/resources/assistant/model/TaskList.kt
@@ -0,0 +1,38 @@
+/*
+ * Nextcloud Android client application
+ *
+ * @author Alper Ozturk
+ * Copyright (C) 2024 Alper Ozturk
+ * Copyright (C) 2024 Nextcloud GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.owncloud.android.lib.resources.assistant.model
+
+data class TaskList (
+    val tasks: List<Task>
+)
+
+data class Task (
+    val id: Long,
+    val type: String,
+    val status: Long,
+    val userId: String,
+    val appId: String,
+    val input: String,
+    val output: String,
+    val identifier: String,
+    val completionExpectedAt: String? = null
+)
