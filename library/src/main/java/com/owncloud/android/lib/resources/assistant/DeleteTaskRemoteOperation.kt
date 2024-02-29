d0cf26915bc8f8fa338ed7b4abb98be77908c845
alperozturk <alper_ozturk@proton.me>
2024-02-29 10:11:56 +0100

    Add Nullable fields
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- library/src/main/java/com/owncloud/android/lib/resources/assistant/DeleteTaskRemoteOperation.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/assistant/DeleteTaskRemoteOperation.kt
@@ -48,7 +48,6 @@ class DeleteTaskRemoteOperation(private val appId: Long) : RemoteOperation<Void>
         } finally {
             postMethod?.releaseConnection()
         }
-
         return result
     }
 
