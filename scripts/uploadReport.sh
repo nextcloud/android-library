51e1a0f1991e62cacc60b21bf6b7f3b40b3a8b47
tobiasKaminsky <tobias@kaminsky.me>
2024-03-04 11:27:29 +0100

    correct url
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- scripts/uploadReport.sh
+++ scripts/uploadReport.sh
@@ -14,16 +14,16 @@ GIT_USERNAME=$7
 GIT_TOKEN=$8
 REMOTE_FOLDER=$ID-$TYPE-$BRANCH-$(date +%H-%M)
 BRANCH_TYPE=$BRANCH-$TYPE
-URL=https://nextcloud.kaminsky.me/remote.php/dav/files/$USER/library-integrationTests
+URL=https://nextcloud.kaminsky.me/remote.php/dav/files/$USER/android-library-integrationTests
 
 # upload logcat
 log_filename=$ID"_logcat.txt.xz"
 log_file="${log_filename}"
-upload_path="https://nextcloud.kaminsky.me/remote.php/webdav/library-logcat/$log_filename"
+upload_path="https://nextcloud.kaminsky.me/remote.php/webdav/android-library-logcat/$log_filename"
 xz logcat.txt
 mv logcat.txt.xz "$log_file"
 curl -u "$USER:$PASS" -X PUT "$upload_path" --upload-file "$log_file"
-echo >&2 "Uploaded logcat to https://www.kaminsky.me/nc-dev/library-logcat/$log_filename"
+echo >&2 "Uploaded logcat to https://www.kaminsky.me/nc-dev/android-library-logcat/$log_filename"
 
 if [ $TYPE = "IT" ]; then
     cd library/build/reports/androidTests/connected
@@ -39,7 +39,7 @@ fi
 find . -type d -exec curl -u $USER:$PASS -X MKCOL $URL/$REMOTE_FOLDER/$(echo {} | sed s#\./##) \;
 find . -type f -exec curl -u $USER:$PASS -X PUT $URL/$REMOTE_FOLDER/$(echo {} | sed s#\./##) --upload-file {} \;
 
-echo "Uploaded failing library tests to https://www.kaminsky.me/nc-dev/library-integrationTests/$REMOTE_FOLDER"
+echo "Uploaded failing library tests to https://www.kaminsky.me/nc-dev/android-library-integrationTests/$REMOTE_FOLDER"
 
-curl -u $GIT_USERNAME:$GIT_TOKEN -X POST https://api.github.com/repos/nextcloud/android-library/issues/$PR_ID/comments -d "{ \"body\" : \"$BRANCH_TYPE test failed: https://www.kaminsky.me/nc-dev/library-integrationTests/$REMOTE_FOLDER/debug/ \" }"
+curl -u $GIT_USERNAME:$GIT_TOKEN -X POST https://api.github.com/repos/nextcloud/android-library/issues/$PR_ID/comments -d "{ \"body\" : \"$BRANCH_TYPE test failed: https://www.kaminsky.me/nc-dev/android-library-integrationTests/$REMOTE_FOLDER/debug/ \" }"
 exit 1
