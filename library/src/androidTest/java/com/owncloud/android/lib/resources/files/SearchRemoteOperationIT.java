cdf247d6e2f32406307d0910bda0d3ed95142045
tobiasKaminsky <tobias@kaminsky.me>
2024-11-01 13:56:45 +0100

    wip
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- library/src/androidTest/java/com/owncloud/android/lib/resources/files/SearchRemoteOperationIT.java
+++ library/src/androidTest/java/com/owncloud/android/lib/resources/files/SearchRemoteOperationIT.java
@@ -99,7 +99,7 @@ public class SearchRemoteOperationIT extends AbstractIT {
         // on newer server we have Talk folder
         int offset = 0;
         if (capability.getVersion().isNewerOrEqual(NextcloudVersion.nextcloud_20) ||
-            capability.getVersion().getMajorVersionNumber() == NextcloudVersion.nextcloud_16.getMajorVersionNumber()) {
+            capability.getVersion().getMajorVersionNumber() == NextcloudVersion.nextcloud_17.getMajorVersionNumber()) {
             offset = 1;
         }
 
@@ -229,7 +229,7 @@ public class SearchRemoteOperationIT extends AbstractIT {
         // on newer server we have Talk folder
         int offset = 0;
         if (capability.getVersion().isNewerOrEqual(NextcloudVersion.nextcloud_20) ||
-            capability.getVersion().getMajorVersionNumber() == NextcloudVersion.nextcloud_16.getMajorVersionNumber()) {
+            capability.getVersion().getMajorVersionNumber() == NextcloudVersion.nextcloud_17.getMajorVersionNumber()) {
             offset = 1;
         }
 
