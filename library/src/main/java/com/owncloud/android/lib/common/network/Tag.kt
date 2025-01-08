3143c628d7b3048d0fd92c623e876623cb9bc81d
tobiasKaminsky <tobias@kaminsky.me>
2025-01-08 09:35:37 +0100

    tag color
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- library/src/main/java/com/owncloud/android/lib/common/network/Tag.kt
+++ library/src/main/java/com/owncloud/android/lib/common/network/Tag.kt
@@ -1,7 +1,8 @@
 /*
  * Nextcloud Android Library
  *
- * SPDX-FileCopyrightText: 2025 Your Name <your@email.com>
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2025 Tobias Kaminsky <tobias@kaminsky.me>
  * SPDX-License-Identifier: MIT
  */
 
