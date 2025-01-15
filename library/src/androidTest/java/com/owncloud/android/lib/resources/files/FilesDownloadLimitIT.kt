abc9c21b0dcc1d388aa906732d2f1b992a03fe03
ZetaTom <70907959+ZetaTom@users.noreply.github.com>
2025-01-15 11:28:06 +0100

    Add FilesDownloadLimitIT
    Signed-off-by: ZetaTom <70907959+ZetaTom@users.noreply.github.com>

---
+++ library/src/androidTest/java/com/owncloud/android/lib/resources/files/FilesDownloadLimitIT.kt
@@ -0,0 +1,97 @@
+/*
+ * Nextcloud Android Library
+ *
+ * SPDX-FileCopyrightText: 2024 ZetaTom <70907959+zetatom@users.noreply.github.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.owncloud.android.lib.resources.files
+
+import com.nextcloud.android.lib.resources.files.GetFilesDownloadLimitRemoteOperation
+import com.nextcloud.android.lib.resources.files.RemoveFilesDownloadLimitRemoteOperation
+import com.nextcloud.android.lib.resources.files.SetFilesDownloadLimitRemoteOperation
+import com.owncloud.android.AbstractIT
+import com.owncloud.android.lib.resources.shares.CreateShareRemoteOperation
+import com.owncloud.android.lib.resources.shares.OCShare
+import com.owncloud.android.lib.resources.shares.ShareType
+import com.owncloud.android.lib.resources.status.GetCapabilitiesRemoteOperation
+import com.owncloud.android.lib.resources.status.OCCapability
+import com.owncloud.android.lib.resources.status.OwnCloudVersion
+import org.junit.Assume.assumeTrue
+import org.junit.Before
+import org.junit.Test
+
+class FilesDownloadLimitIT : AbstractIT() {
+    @Before
+    fun before() {
+        testOnlyOnServer(OwnCloudVersion.nextcloud_30)
+        assumeTrue(getCapability().filesDownloadLimit.isTrue)
+    }
+
+    @Test
+    fun getDefaultLimit() {
+        val defaultLimit = getCapability().filesDownloadLimitDefault
+        assert(defaultLimit == 0)
+    }
+
+    @Test
+    fun downloadLimit() {
+        val share = createTestShare()
+        val limit = 5
+
+        val resultSet = SetFilesDownloadLimitRemoteOperation(share.token!!, limit).execute(nextcloudClient)
+        assert(resultSet.isSuccess)
+
+        shortSleep()
+
+        val resultGet1 = GetFilesDownloadLimitRemoteOperation(REMOTE_PATH, false).execute(client)
+        assert(resultGet1.isSuccess)
+        assert(resultGet1.resultData.size == 1)
+        assert(resultGet1.resultData.first().token == share.token)
+        assert(resultGet1.resultData.first().limit == limit)
+        assert(resultGet1.resultData.first().count == 0)
+
+        shortSleep()
+
+        val resultRemove = RemoveFilesDownloadLimitRemoteOperation(share.token!!).execute(nextcloudClient)
+        assert(resultRemove.isSuccess)
+
+        shortSleep()
+
+        val resultGet2 = GetFilesDownloadLimitRemoteOperation(REMOTE_PATH, false).execute(client)
+        assert(resultGet2.isSuccess)
+        assert(resultGet2.resultData.isEmpty())
+    }
+
+    private fun getCapability(): OCCapability =
+        GetCapabilitiesRemoteOperation().execute(nextcloudClient).singleData as OCCapability
+
+    private fun createTestShare(): OCShare {
+        val localPath = createFile("test")
+
+        assert(
+            UploadFileRemoteOperation(localPath, REMOTE_PATH, "text/plain", RANDOM_MTIME)
+                .execute(client)
+                .isSuccess
+        )
+
+        val result =
+            CreateShareRemoteOperation(
+                REMOTE_PATH,
+                ShareType.PUBLIC_LINK,
+                "",
+                false,
+                "",
+                1
+            ).execute(client)
+
+        assert(result.isSuccess)
+        val share = result.resultData.first()
+        assert(share.token != null)
+        return share
+    }
+
+    companion object {
+        private const val REMOTE_PATH = "/downloadLimits.txt"
+    }
+}
