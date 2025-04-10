82e5a94b8a17dc322bf290f0509518acbb7b3750
alperozturk <alper_ozturk@proton.me>
2025-04-10 12:35:40 +0200

    remove unnecessary logic
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- library/src/main/java/com/owncloud/android/lib/resources/shares/attributes/ShareAttributesSerializer.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/shares/attributes/ShareAttributesSerializer.kt
@@ -7,73 +7,28 @@
 
 package com.owncloud.android.lib.resources.shares.attributes
 
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.SerializationException
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.descriptors.buildClassSerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.json.JsonDecoder
-import kotlinx.serialization.json.JsonEncoder
-import kotlinx.serialization.json.JsonPrimitive
-import kotlinx.serialization.json.booleanOrNull
-import kotlinx.serialization.json.buildJsonObject
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import java.lang.reflect.Type
 
 /**
  * Custom serializer for the ShareAttributes class.
  * This handles the deserialization and serialization of the ShareAttributes data class.
  * Since Nextcloud 30, the enabled key have bee renamed to value and supports more than boolean.
  */
-object ShareAttributesSerializer : KSerializer<ShareAttributes> {
-    override val descriptor: SerialDescriptor =
-        buildClassSerialDescriptor("ShareAttributes") {
-            element("scope", PrimitiveSerialDescriptor("scope", PrimitiveKind.STRING))
-            element("key", PrimitiveSerialDescriptor("key", PrimitiveKind.STRING))
-            element("enabled", PrimitiveSerialDescriptor("enabled", PrimitiveKind.BOOLEAN), isOptional = true)
-            element("value", PrimitiveSerialDescriptor("value", PrimitiveKind.BOOLEAN), isOptional = true)
+class ShareAttributesDeserializer: JsonDeserializer<ShareAttributes> {
+    override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): ShareAttributes? {
+        val jsonObject = json?.asJsonObject
+        val scope = jsonObject?.get("scope")?.asString ?: ""
+        val key = jsonObject?.get("key")?.asString ?: ""
+
+        val status = when {
+            jsonObject?.has("enabled") == true -> jsonObject.get("enabled").asBoolean
+            jsonObject?.has("value") == true -> jsonObject.get("value").asBoolean
+            else -> false
         }
 
-    override fun deserialize(decoder: Decoder): ShareAttributes {
-        val input =
-            decoder as? JsonDecoder
-                ?: throw SerializationException("Expected JsonDecoder")
-
-        val jsonObject = input.decodeJsonElement().jsonObject
-        val scope =
-            jsonObject["scope"]?.jsonPrimitive?.content
-                ?: throw SerializationException("Missing 'scope'")
-        val key =
-            jsonObject["key"]?.jsonPrimitive?.content
-                ?: throw SerializationException("Missing 'key'")
-
-        // Check for the 'enabled' or 'value' field. If neither is found, throw an exception.
-        val isEnabled =
-            jsonObject["enabled"]?.jsonPrimitive?.booleanOrNull
-                ?: jsonObject["value"]?.jsonPrimitive?.booleanOrNull
-                ?: throw SerializationException("Missing 'enabled' or 'value'")
-
-        return ShareAttributes(scope, key, isEnabled)
-    }
-
-    override fun serialize(
-        encoder: Encoder,
-        value: ShareAttributes
-    ) {
-        val output =
-            encoder as? JsonEncoder
-                ?: throw SerializationException("Expected JsonEncoder")
-
-        val json =
-            buildJsonObject {
-                put("scope", JsonPrimitive(value.scope))
-                put("key", JsonPrimitive(value.key))
-                put("value", JsonPrimitive(value.isEnabled))
-            }
-
-        output.encodeJsonElement(json)
+        return ShareAttributes(scope, key, status)
     }
 }
