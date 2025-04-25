50f678be59a2515a1b298f5c7aaaf46aaea75b0b
alperozturk <alper_ozturk@proton.me>
2025-04-25 15:24:06 +0200

    fix license
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- library/src/androidTest/java/com/owncloud/android/ResponseFormatDetectorTests.kt
+++ library/src/androidTest/java/com/owncloud/android/ResponseFormatDetectorTests.kt
@@ -1,8 +1,8 @@
 /*
- * Nextcloud - Android Client
+ * Nextcloud Android Library
  *
  * SPDX-FileCopyrightText: 2025 Alper Ozturk <alper.ozturk@nextcloud.com>
- * SPDX-License-Identifier: AGPL-3.0-or-later
+ * SPDX-License-Identifier: MIT
  */
 
 package com.owncloud.android
