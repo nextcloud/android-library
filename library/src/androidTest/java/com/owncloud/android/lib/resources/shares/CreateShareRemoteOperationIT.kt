f30d7e39cab9aca8dcc9faf51c820d08d242acff
alperozturk <alper_ozturk@proton.me>
2025-05-06 11:59:58 +0200

    add attributes test
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- library/src/androidTest/java/com/owncloud/android/lib/resources/shares/CreateShareRemoteOperationIT.kt
+++ library/src/androidTest/java/com/owncloud/android/lib/resources/shares/CreateShareRemoteOperationIT.kt
@@ -9,11 +9,13 @@ package com.owncloud.android.lib.resources.shares
 
 import com.owncloud.android.AbstractIT
 import com.owncloud.android.lib.resources.files.CreateFolderRemoteOperation
+import com.owncloud.android.lib.resources.shares.attributes.ShareAttributes
+import com.owncloud.android.lib.resources.shares.attributes.ShareAttributesJsonHandler
 import com.owncloud.android.lib.resources.status.GetStatusRemoteOperation
 import com.owncloud.android.lib.resources.status.NextcloudVersion
 import com.owncloud.android.lib.resources.status.OwnCloudVersion
-import org.junit.Assert
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Assume
 import org.junit.Before
 import org.junit.Test
@@ -22,41 +24,58 @@ class CreateShareRemoteOperationIT : AbstractIT() {
     @Before
     fun before() {
         val result = GetStatusRemoteOperation(context).execute(client)
-        Assert.assertTrue(result.isSuccess)
+        assertTrue(result.isSuccess)
         val data = result.data as ArrayList<Any>
         val ownCloudVersion = data[0] as OwnCloudVersion
         Assume.assumeTrue(ownCloudVersion.isNewerOrEqual(NextcloudVersion.nextcloud_24))
     }
 
     @Test
+    fun createShareWithNoteAndAttributes() {
+        val attributes = ShareAttributesJsonHandler.toJson(
+            listOf(ShareAttributes.createDownloadAttributes(true))
+        )
+        val note = "Note with attributes"
+        val path = "/shareWithAttributes/"
+
+        createFolder(path)
+        val share = createShare(path, "admin", note, attributes)
+        assertEquals(note, share.note)
+    }
+
+    @Test
     fun createShareWithNote() {
         val note = "This is the note"
+        val path = "/share/"
 
-        Assert.assertTrue(
-            CreateFolderRemoteOperation(
-                "/share/",
-                true
-            ).execute(client).isSuccess
-        )
-
-        // share folder to user "admin"
-        val sut =
-            CreateShareRemoteOperation(
-                "/share/",
-                ShareType.USER,
-                "admin",
-                false,
-                "",
-                OCShare.MAXIMUM_PERMISSIONS_FOR_FOLDER,
-                true,
-                note,
-                null
-            ).execute(client)
-
-        junit.framework.Assert.assertTrue(sut.isSuccess)
-
-        val share = sut.resultData[0]
-
+        createFolder(path)
+        val share = createShare(path, "admin", note)
         assertEquals(note, share.note)
+    }
+
+    private fun createFolder(path: String) {
+        assertTrue(CreateFolderRemoteOperation(path, true).execute(client).isSuccess)
+    }
+
+    private fun createShare(
+        path: String,
+        accountName: String,
+        note: String,
+        attributes: String? = null
+    ): OCShare {
+        val operation = CreateShareRemoteOperation(
+            path,
+            ShareType.USER,
+            accountName,
+            false,
+            "",
+            OCShare.MAXIMUM_PERMISSIONS_FOR_FOLDER,
+            true,
+            note,
+            attributes
+        )
+        val result = operation.execute(client)
+        assertTrue(result.isSuccess)
+        return result.resultData[0]
     }
 }
