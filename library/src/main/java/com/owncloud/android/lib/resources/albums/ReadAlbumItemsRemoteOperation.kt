6a6c8d05f4e2ecedec0d015471edcf292b8c5437
A117870935 <surinder.kumar@t-systems.com>
2025-06-04 11:32:14 +0530

    Operations migrated to kotlin.

--- library/src/main/java/com/owncloud/android/lib/resources/albums/ReadAlbumItemsRemoteOperation.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/albums/ReadAlbumItemsRemoteOperation.kt
@@ -4,96 +4,99 @@
  * SPDX-FileCopyrightText: 2025 TSI-mc <surinder.kumar@t-systems.com>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+package com.owncloud.android.lib.resources.albums
 
-package com.owncloud.android.lib.resources.albums;
+import com.nextcloud.common.SessionTimeOut
+import com.nextcloud.common.defaultSessionTimeOut
+import com.owncloud.android.lib.common.OwnCloudClient
+import com.owncloud.android.lib.common.network.WebdavUtils
+import com.owncloud.android.lib.common.operations.RemoteOperation
+import com.owncloud.android.lib.common.operations.RemoteOperationResult
+import com.owncloud.android.lib.common.utils.Log_OC
+import com.owncloud.android.lib.common.utils.WebDavFileUtils
+import com.owncloud.android.lib.resources.albums.ReadAlbumItemsRemoteOperation
+import com.owncloud.android.lib.resources.files.model.RemoteFile
+import org.apache.commons.httpclient.HttpStatus
+import org.apache.jackrabbit.webdav.DavConstants
+import org.apache.jackrabbit.webdav.client.methods.PropFindMethod
 
-import com.nextcloud.common.SessionTimeOut;
-import com.nextcloud.common.SessionTimeOutKt;
-import com.owncloud.android.lib.common.OwnCloudClient;
-import com.owncloud.android.lib.common.network.WebdavUtils;
-import com.owncloud.android.lib.common.operations.RemoteOperation;
-import com.owncloud.android.lib.common.operations.RemoteOperationResult;
-import com.owncloud.android.lib.common.utils.Log_OC;
-import com.owncloud.android.lib.common.utils.WebDavFileUtils;
-import com.owncloud.android.lib.resources.files.model.RemoteFile;
-
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.jackrabbit.webdav.DavConstants;
-import org.apache.jackrabbit.webdav.MultiStatus;
-import org.apache.jackrabbit.webdav.client.methods.PropFindMethod;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class ReadAlbumItemsRemoteOperation extends RemoteOperation<List<RemoteFile>> {
-
-    private static final String TAG = ReadAlbumItemsRemoteOperation.class.getSimpleName();
-    private final String mRemotePath;
-    private final SessionTimeOut sessionTimeOut;
-
-    public ReadAlbumItemsRemoteOperation(String mRemotePath) {
-        this(mRemotePath, SessionTimeOutKt.getDefaultSessionTimeOut());
-    }
-
-    public ReadAlbumItemsRemoteOperation(String mRemotePath, SessionTimeOut sessionTimeOut) {
-        this.mRemotePath = mRemotePath;
-        this.sessionTimeOut = sessionTimeOut;
-    }
-
-    protected RemoteOperationResult<List<RemoteFile>> run(OwnCloudClient client) {
-        RemoteOperationResult<List<RemoteFile>> result = null;
-        PropFindMethod query = null;
-        String url = client.getBaseUri() + "/remote.php/dav/photos/" + client.getUserId() + "/albums" + WebdavUtils.encodePath(mRemotePath);
+class ReadAlbumItemsRemoteOperation @JvmOverloads constructor(
+    private val mRemotePath: String,
+    private val sessionTimeOut: SessionTimeOut = defaultSessionTimeOut
+) :
+    RemoteOperation<List<RemoteFile>>() {
+    @Deprecated("Deprecated in Java")
+    override fun run(client: OwnCloudClient): RemoteOperationResult<List<RemoteFile>> {
+        var result: RemoteOperationResult<List<RemoteFile>>? = null
+        var query: PropFindMethod? = null
+        val url = "${client.baseUri}/remote.php/dav/photos/${client.userId}/albums${
+            WebdavUtils.encodePath(
+                mRemotePath
+            )
+        }"
         try {
             // remote request
-            query = new PropFindMethod(url,
-                WebdavUtils.getAllPropSet(), // PropFind Properties
-                DavConstants.DEPTH_1);
-            int status = client.executeMethod(query, sessionTimeOut.getReadTimeOut(), sessionTimeOut.getConnectionTimeOut());
+            query = PropFindMethod(
+                url,
+                WebdavUtils.getAllPropSet(),  // PropFind Properties
+                DavConstants.DEPTH_1
+            )
+            val status = client.executeMethod(
+                query,
+                sessionTimeOut.readTimeOut,
+                sessionTimeOut.connectionTimeOut
+            )
 
             // check and process response
-            boolean isSuccess = (status == HttpStatus.SC_MULTI_STATUS || status == HttpStatus.SC_OK);
+            val isSuccess = (status == HttpStatus.SC_MULTI_STATUS || status == HttpStatus.SC_OK)
 
             if (isSuccess) {
                 // get data from remote folder
-                MultiStatus dataInServer = query.getResponseBodyAsMultiStatus();
-                List<RemoteFile> mFolderAndFiles = new WebDavFileUtils().readAlbumData(dataInServer, client);
+                val dataInServer = query.responseBodyAsMultiStatus
+                val mFolderAndFiles = WebDavFileUtils().readAlbumData(dataInServer, client)
 
                 // Result of the operation
-                result = new RemoteOperationResult<>(true, query);
+                result = RemoteOperationResult(true, query)
                 // Add data to the result
-                if (result.isSuccess()) {
-                    result.setResultData(mFolderAndFiles);
+                if (result.isSuccess) {
+                    result.resultData = mFolderAndFiles
                 }
             } else {
                 // synchronization failed
-                client.exhaustResponse(query.getResponseBodyAsStream());
-                result = new RemoteOperationResult<>(false, query);
+                client.exhaustResponse(query.responseBodyAsStream)
+                result = RemoteOperationResult(false, query)
             }
-        } catch (Exception e) {
-            result = new RemoteOperationResult<>(e);
+        } catch (e: Exception) {
+            result = RemoteOperationResult(e)
         } finally {
-            if (query != null) {
-                query.releaseConnection(); // let the connection available for other methods
-            }
+            query?.releaseConnection()
 
             if (result == null) {
-                result = new RemoteOperationResult(new Exception("unknown error"));
-                Log_OC.e(TAG, "Synchronized " + mRemotePath + ": failed");
+                result = RemoteOperationResult<List<RemoteFile>>(Exception("unknown error"))
+                Log_OC.e(
+                    TAG,
+                    "Synchronized $mRemotePath: failed"
+                )
             } else {
-                if (result.isSuccess()) {
-                    Log_OC.i(TAG, "Synchronized " + mRemotePath + ": " + result.getLogMessage());
+                if (result.isSuccess) {
+                    Log_OC.i(TAG, "Synchronized $mRemotePath : ${result.logMessage}")
                 } else {
-                    if (result.isException()) {
-                        Log_OC.e(TAG, "Synchronized " + mRemotePath + ": " + result.getLogMessage(),
-                            result.getException());
+                    if (result.isException) {
+                        Log_OC.e(
+                            TAG, "Synchronized $mRemotePath : ${result.logMessage}",
+                            result.exception
+                        )
                     } else {
-                        Log_OC.e(TAG, "Synchronized " + mRemotePath + ": " + result.getLogMessage());
+                        Log_OC.e(TAG, "Synchronized $mRemotePath : ${result.logMessage}")
                     }
                 }
             }
         }
 
-        return result;
+        return result
+    }
+
+    companion object {
+        private val TAG: String = ReadAlbumItemsRemoteOperation::class.java.simpleName
     }
 }
--- library/src/main/java/com/owncloud/android/lib/resources/albums/RemoveAlbumFileRemoteOperation.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/albums/RemoveAlbumFileRemoteOperation.kt
@@ -4,59 +4,55 @@
  * SPDX-FileCopyrightText: 2025 TSI-mc <surinder.kumar@t-systems.com>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+package com.owncloud.android.lib.resources.albums
 
-package com.owncloud.android.lib.resources.albums;
+import android.net.Uri
+import com.nextcloud.common.SessionTimeOut
+import com.nextcloud.common.defaultSessionTimeOut
+import com.owncloud.android.lib.common.OwnCloudClient
+import com.owncloud.android.lib.common.operations.RemoteOperation
+import com.owncloud.android.lib.common.operations.RemoteOperationResult
+import com.owncloud.android.lib.common.utils.Log_OC
+import com.owncloud.android.lib.resources.files.RemoveFileRemoteOperation
+import org.apache.commons.httpclient.HttpStatus
+import org.apache.jackrabbit.webdav.client.methods.DeleteMethod
 
-import android.net.Uri;
-
-import com.nextcloud.common.SessionTimeOut;
-import com.nextcloud.common.SessionTimeOutKt;
-import com.owncloud.android.lib.common.OwnCloudClient;
-import com.owncloud.android.lib.common.operations.RemoteOperation;
-import com.owncloud.android.lib.common.operations.RemoteOperationResult;
-import com.owncloud.android.lib.common.utils.Log_OC;
-import com.owncloud.android.lib.resources.files.RemoveFileRemoteOperation;
-
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.jackrabbit.webdav.client.methods.DeleteMethod;
-
-public class RemoveAlbumFileRemoteOperation extends RemoteOperation {
-    private static final String TAG = RemoveFileRemoteOperation.class.getSimpleName();
-    private final String mRemotePath;
-    private final SessionTimeOut sessionTimeOut;
-
-    public RemoveAlbumFileRemoteOperation(String remotePath) {
-        this(remotePath, SessionTimeOutKt.getDefaultSessionTimeOut());
-    }
-
-    public RemoveAlbumFileRemoteOperation(String remotePath, SessionTimeOut sessionTimeOut) {
-        this.mRemotePath = remotePath;
-        this.sessionTimeOut = sessionTimeOut;
-    }
-
-    protected RemoteOperationResult run(OwnCloudClient client) {
-        RemoteOperationResult result;
-        DeleteMethod delete = null;
-        String webDavUrl = client.getDavUri().toString()+"/photos/";
-        String encodedPath = (client.getUserId() + Uri.encode(this.mRemotePath)).replace("%2F", "/");
-        String fullFilePath = webDavUrl + encodedPath;
+class RemoveAlbumFileRemoteOperation @JvmOverloads constructor(
+    private val mRemotePath: String,
+    private val sessionTimeOut: SessionTimeOut = defaultSessionTimeOut
+) :
+    RemoteOperation<Any>() {
+    @Deprecated("Deprecated in Java")
+    override fun run(client: OwnCloudClient): RemoteOperationResult<Any> {
+        var result: RemoteOperationResult<Any>
+        var delete: DeleteMethod? = null
+        val webDavUrl = "${client.davUri}/photos/"
+        val encodedPath = ("${client.userId}${Uri.encode(this.mRemotePath)}").replace("%2F", "/")
+        val fullFilePath = "$webDavUrl$encodedPath"
 
         try {
-            delete = new DeleteMethod(fullFilePath);
-            int status = client.executeMethod(delete, this.sessionTimeOut.getReadTimeOut(), this.sessionTimeOut.getConnectionTimeOut());
-            delete.getResponseBodyAsString();
-            result = new RemoteOperationResult(delete.succeeded() || status == HttpStatus.SC_NOT_FOUND, delete);
-            Log_OC.i(TAG, "Remove " + this.mRemotePath + ": " + result.getLogMessage());
-        } catch (Exception e) {
-            result = new RemoteOperationResult(e);
-            Log_OC.e(TAG, "Remove " + this.mRemotePath + ": " + result.getLogMessage(), e);
+            delete = DeleteMethod(fullFilePath)
+            val status = client.executeMethod(
+                delete,
+                sessionTimeOut.readTimeOut, sessionTimeOut.connectionTimeOut
+            )
+            delete.responseBodyAsString
+            result = RemoteOperationResult<Any>(
+                delete.succeeded() || status == HttpStatus.SC_NOT_FOUND,
+                delete
+            )
+            Log_OC.i(TAG, "Remove ${this.mRemotePath} : ${result.logMessage}")
+        } catch (e: Exception) {
+            result = RemoteOperationResult<Any>(e)
+            Log_OC.e(TAG, "Remove ${this.mRemotePath} : ${result.logMessage}", e)
         } finally {
-            if (delete != null) {
-                delete.releaseConnection();
-            }
-
+            delete?.releaseConnection()
         }
 
-        return result;
+        return result
+    }
+
+    companion object {
+        private val TAG: String = RemoveFileRemoteOperation::class.java.simpleName
     }
 }
