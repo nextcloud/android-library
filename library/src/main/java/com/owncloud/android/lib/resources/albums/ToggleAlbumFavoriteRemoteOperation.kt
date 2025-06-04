9958d9f43344d317281dd0e79dacc7cde2225008
A117870935 <surinder.kumar@t-systems.com>
2025-06-04 11:32:14 +0530

    Operations migrated to kotlin.
    Signed-off-by: Surinder Kumar <surinder.kumar@t-systems.com>

--- library/src/main/java/com/owncloud/android/lib/resources/albums/ToggleAlbumFavoriteRemoteOperation.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/albums/ToggleAlbumFavoriteRemoteOperation.kt
@@ -4,77 +4,70 @@
  * SPDX-FileCopyrightText: 2025 TSI-mc <surinder.kumar@t-systems.com>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+package com.owncloud.android.lib.resources.albums
 
-package com.owncloud.android.lib.resources.albums;
+import android.net.Uri
+import com.nextcloud.common.SessionTimeOut
+import com.nextcloud.common.defaultSessionTimeOut
+import com.owncloud.android.lib.common.OwnCloudClient
+import com.owncloud.android.lib.common.network.WebdavEntry
+import com.owncloud.android.lib.common.operations.RemoteOperation
+import com.owncloud.android.lib.common.operations.RemoteOperationResult
+import org.apache.commons.httpclient.HttpStatus
+import org.apache.jackrabbit.webdav.client.methods.PropPatchMethod
+import org.apache.jackrabbit.webdav.property.DavPropertyNameSet
+import org.apache.jackrabbit.webdav.property.DavPropertySet
+import org.apache.jackrabbit.webdav.property.DefaultDavProperty
+import org.apache.jackrabbit.webdav.xml.Namespace
+import java.io.IOException
 
-import android.net.Uri;
-
-import com.nextcloud.common.SessionTimeOut;
-import com.nextcloud.common.SessionTimeOutKt;
-import com.owncloud.android.lib.common.OwnCloudClient;
-import com.owncloud.android.lib.common.network.WebdavEntry;
-import com.owncloud.android.lib.common.operations.RemoteOperation;
-import com.owncloud.android.lib.common.operations.RemoteOperationResult;
-
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.jackrabbit.webdav.client.methods.PropPatchMethod;
-import org.apache.jackrabbit.webdav.property.DavPropertyNameSet;
-import org.apache.jackrabbit.webdav.property.DavPropertySet;
-import org.apache.jackrabbit.webdav.property.DefaultDavProperty;
-import org.apache.jackrabbit.webdav.xml.Namespace;
-
-import java.io.IOException;
-
-public class ToggleAlbumFavoriteRemoteOperation extends RemoteOperation {
-    private boolean makeItFavorited;
-    private String filePath;
-    private final SessionTimeOut sessionTimeOut;
-
-    public ToggleAlbumFavoriteRemoteOperation(boolean makeItFavorited, String filePath) {
-        this(makeItFavorited, filePath, SessionTimeOutKt.getDefaultSessionTimeOut());
-    }
-
-    public ToggleAlbumFavoriteRemoteOperation(boolean makeItFavorited, String filePath, SessionTimeOut sessionTimeOut) {
-        this.makeItFavorited = makeItFavorited;
-        this.filePath = filePath;
-        this.sessionTimeOut = sessionTimeOut;
-    }
-
-    protected RemoteOperationResult run(OwnCloudClient client) {
-        RemoteOperationResult result;
-        PropPatchMethod propPatchMethod = null;
-        DavPropertySet newProps = new DavPropertySet();
-        DavPropertyNameSet removeProperties = new DavPropertyNameSet();
+class ToggleAlbumFavoriteRemoteOperation @JvmOverloads constructor(
+    private val makeItFavorited: Boolean,
+    private val filePath: String,
+    private val sessionTimeOut: SessionTimeOut = defaultSessionTimeOut
+) :
+    RemoteOperation<Any>() {
+    @Deprecated("Deprecated in Java")
+    override fun run(client: OwnCloudClient): RemoteOperationResult<Any> {
+        var result: RemoteOperationResult<Any>
+        var propPatchMethod: PropPatchMethod? = null
+        val newProps = DavPropertySet()
+        val removeProperties = DavPropertyNameSet()
         if (this.makeItFavorited) {
-            DefaultDavProperty<String> favoriteProperty = new DefaultDavProperty("oc:favorite", "1", Namespace.getNamespace(WebdavEntry.NAMESPACE_OC));
-            newProps.add(favoriteProperty);
+            val favoriteProperty = DefaultDavProperty<Any>(
+                "oc:favorite",
+                "1",
+                Namespace.getNamespace(WebdavEntry.NAMESPACE_OC)
+            )
+            newProps.add(favoriteProperty)
         } else {
-            removeProperties.add("oc:favorite", Namespace.getNamespace(WebdavEntry.NAMESPACE_OC));
+            removeProperties.add("oc:favorite", Namespace.getNamespace(WebdavEntry.NAMESPACE_OC))
         }
 
-        String webDavUrl = client.getDavUri().toString() + "/photos/";
-        String encodedPath = (client.getUserId() + Uri.encode(this.filePath)).replace("%2F", "/");
-        String fullFilePath = webDavUrl + encodedPath;
+        val webDavUrl = "${client.davUri}/photos/"
+        val encodedPath = ("${client.userId}${Uri.encode(this.filePath)}").replace("%2F", "/")
+        val fullFilePath = "$webDavUrl$encodedPath"
 
         try {
-            propPatchMethod = new PropPatchMethod(fullFilePath, newProps, removeProperties);
-            int status = client.executeMethod(propPatchMethod, sessionTimeOut.getReadTimeOut(), sessionTimeOut.getConnectionTimeOut());
-            boolean isSuccess = (status == HttpStatus.SC_MULTI_STATUS || status == HttpStatus.SC_OK);
+            propPatchMethod = PropPatchMethod(fullFilePath, newProps, removeProperties)
+            val status = client.executeMethod(
+                propPatchMethod,
+                sessionTimeOut.readTimeOut,
+                sessionTimeOut.connectionTimeOut
+            )
+            val isSuccess = (status == HttpStatus.SC_MULTI_STATUS || status == HttpStatus.SC_OK)
             if (isSuccess) {
-                result = new RemoteOperationResult(true, status, propPatchMethod.getResponseHeaders());
+                result = RemoteOperationResult<Any>(true, status, propPatchMethod.responseHeaders)
             } else {
-                client.exhaustResponse(propPatchMethod.getResponseBodyAsStream());
-                result = new RemoteOperationResult(false, status, propPatchMethod.getResponseHeaders());
+                client.exhaustResponse(propPatchMethod.responseBodyAsStream)
+                result = RemoteOperationResult<Any>(false, status, propPatchMethod.responseHeaders)
             }
-        } catch (IOException e) {
-            result = new RemoteOperationResult(e);
+        } catch (e: IOException) {
+            result = RemoteOperationResult<Any>(e)
         } finally {
-            if (propPatchMethod != null) {
-                propPatchMethod.releaseConnection();
-            }
-
+            propPatchMethod?.releaseConnection()
         }
 
-        return result;
+        return result
     }
 }
