80c4fe060a447175a6c2bcae5dbd6674b4c91fea
A117870935 <surinder.kumar@t-systems.com>
2025-06-04 11:32:14 +0530

    Operations migrated to kotlin.
    Signed-off-by: Surinder Kumar <surinder.kumar@t-systems.com>

--- library/src/main/java/com/owncloud/android/lib/resources/albums/RemoveAlbumRemoteOperation.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/albums/RemoveAlbumRemoteOperation.kt
@@ -4,61 +4,64 @@
  * SPDX-FileCopyrightText: 2025 TSI-mc <surinder.kumar@t-systems.com>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+package com.owncloud.android.lib.resources.albums
 
-package com.owncloud.android.lib.resources.albums;
+import com.nextcloud.common.SessionTimeOut
+import com.nextcloud.common.defaultSessionTimeOut
+import com.owncloud.android.lib.common.OwnCloudClient
+import com.owncloud.android.lib.common.network.WebdavUtils
+import com.owncloud.android.lib.common.operations.RemoteOperation
+import com.owncloud.android.lib.common.operations.RemoteOperationResult
+import com.owncloud.android.lib.common.utils.Log_OC
+import com.owncloud.android.lib.resources.albums.RemoveAlbumRemoteOperation
+import org.apache.commons.httpclient.HttpStatus
+import org.apache.jackrabbit.webdav.client.methods.DeleteMethod
 
-import com.nextcloud.common.SessionTimeOut;
-import com.nextcloud.common.SessionTimeOutKt;
-import com.owncloud.android.lib.common.OwnCloudClient;
-import com.owncloud.android.lib.common.network.WebdavUtils;
-import com.owncloud.android.lib.common.operations.RemoteOperation;
-import com.owncloud.android.lib.common.operations.RemoteOperationResult;
-import com.owncloud.android.lib.common.utils.Log_OC;
-
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.jackrabbit.webdav.client.methods.DeleteMethod;
-
-public class RemoveAlbumRemoteOperation extends RemoteOperation {
-    private static final String TAG = RemoveAlbumRemoteOperation.class.getSimpleName();
-    private final String albumName;
-    private final SessionTimeOut sessionTimeOut;
-
-    public RemoveAlbumRemoteOperation(String albumName) {
-        this(albumName, SessionTimeOutKt.getDefaultSessionTimeOut());
-    }
-
-    public RemoveAlbumRemoteOperation(String albumName, SessionTimeOut sessionTimeOut) {
-        this.albumName = albumName;
-        this.sessionTimeOut = sessionTimeOut;
-    }
-
+class RemoveAlbumRemoteOperation @JvmOverloads constructor(
+    private val albumName: String,
+    private val sessionTimeOut: SessionTimeOut = defaultSessionTimeOut
+) :
+    RemoteOperation<Any>() {
     /**
      * Performs the operation.
      *
      * @param client Client object to communicate with the remote ownCloud server.
      */
-    @Override
-    protected RemoteOperationResult run(OwnCloudClient client) {
-        RemoteOperationResult result;
-        DeleteMethod delete = null;
+    @Deprecated("Deprecated in Java")
+    override fun run(client: OwnCloudClient): RemoteOperationResult<Any> {
+        var result: RemoteOperationResult<Any>
+        var delete: DeleteMethod? = null
 
         try {
-            delete = new DeleteMethod(client.getBaseUri() + "/remote.php/dav/photos/" + client.getUserId() + "/albums" + WebdavUtils.encodePath(albumName));
-            int status = client.executeMethod(delete, sessionTimeOut.getReadTimeOut(), sessionTimeOut.getConnectionTimeOut());
-            delete.getResponseBodyAsString();
-            result = new RemoteOperationResult(delete.succeeded() || status == HttpStatus.SC_NOT_FOUND, delete);
-            Log_OC.i(TAG, "Remove " + this.albumName + ": " + result.getLogMessage());
-        } catch (Exception e) {
-            result = new RemoteOperationResult(e);
-            Log_OC.e(TAG, "Remove " + this.albumName + ": " + result.getLogMessage(), e);
+            delete = DeleteMethod(
+                "${client.baseUri}/remote.php/dav/photos/${client.userId}/albums${
+                    WebdavUtils.encodePath(
+                        albumName
+                    )
+                }"
+            )
+            val status = client.executeMethod(
+                delete,
+                sessionTimeOut.readTimeOut,
+                sessionTimeOut.connectionTimeOut
+            )
+            delete.responseBodyAsString
+            result = RemoteOperationResult<Any>(
+                delete.succeeded() || status == HttpStatus.SC_NOT_FOUND,
+                delete
+            )
+            Log_OC.i(TAG, "Remove ${this.albumName} : ${result.logMessage}")
+        } catch (e: Exception) {
+            result = RemoteOperationResult<Any>(e)
+            Log_OC.e(TAG, "Remove ${this.albumName} : ${result.logMessage}", e)
         } finally {
-            if (delete != null) {
-                delete.releaseConnection();
-            }
-
+            delete?.releaseConnection()
         }
 
-        return result;
+        return result
     }
 
+    companion object {
+        private val TAG: String = RemoveAlbumRemoteOperation::class.java.simpleName
+    }
 }
