e45d42a3865555126f50c40ea737622d4ddd34dc
A117870935 <surinder.kumar@t-systems.com>
2025-06-06 14:12:51 +0530

    Fix
    Signed-off-by: Surinder Kumar <surinder.kumar@t-systems.com>

--- library/src/main/java/com/owncloud/android/lib/resources/albums/ReadAlbumItemsRemoteOperation.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/albums/ReadAlbumItemsRemoteOperation.kt
@@ -19,83 +19,77 @@ import org.apache.commons.httpclient.HttpStatus
 import org.apache.jackrabbit.webdav.DavConstants
 import org.apache.jackrabbit.webdav.client.methods.PropFindMethod
 
-class ReadAlbumItemsRemoteOperation @JvmOverloads constructor(
-    private val mRemotePath: String,
-    private val sessionTimeOut: SessionTimeOut = defaultSessionTimeOut
-) :
-    RemoteOperation<List<RemoteFile>>() {
-    @Deprecated("Deprecated in Java")
-    override fun run(client: OwnCloudClient): RemoteOperationResult<List<RemoteFile>> {
-        var result: RemoteOperationResult<List<RemoteFile>>? = null
-        var query: PropFindMethod? = null
-        val url = "${client.baseUri}/remote.php/dav/photos/${client.userId}/albums${
-            WebdavUtils.encodePath(
-                mRemotePath
-            )
-        }"
-        try {
-            // remote request
-            query = PropFindMethod(
-                url,
-                WebdavUtils.getAllPropSet(),  // PropFind Properties
-                DavConstants.DEPTH_1
-            )
-            val status = client.executeMethod(
-                query,
-                sessionTimeOut.readTimeOut,
-                sessionTimeOut.connectionTimeOut
-            )
-
-            // check and process response
-            val isSuccess = (status == HttpStatus.SC_MULTI_STATUS || status == HttpStatus.SC_OK)
-
-            if (isSuccess) {
-                // get data from remote folder
-                val dataInServer = query.responseBodyAsMultiStatus
-                val mFolderAndFiles = WebDavFileUtils().readAlbumData(dataInServer, client)
-
-                // Result of the operation
-                result = RemoteOperationResult(true, query)
-                // Add data to the result
-                if (result.isSuccess) {
-                    result.resultData = mFolderAndFiles
-                }
-            } else {
-                // synchronization failed
-                client.exhaustResponse(query.responseBodyAsStream)
-                result = RemoteOperationResult(false, query)
-            }
-        } catch (e: Exception) {
-            result = RemoteOperationResult(e)
-        } finally {
-            query?.releaseConnection()
-
-            if (result == null) {
-                result = RemoteOperationResult<List<RemoteFile>>(Exception("unknown error"))
-                Log_OC.e(
-                    TAG,
-                    "Synchronized $mRemotePath: failed"
+class ReadAlbumItemsRemoteOperation
+    @JvmOverloads
+    constructor(
+        private val mRemotePath: String,
+        private val sessionTimeOut: SessionTimeOut = defaultSessionTimeOut
+    ) : RemoteOperation<List<RemoteFile>>() {
+        @Deprecated("Deprecated in Java")
+        @Suppress("TooGenericExceptionCaught")
+        override fun run(client: OwnCloudClient): RemoteOperationResult<List<RemoteFile>> {
+            var result: RemoteOperationResult<List<RemoteFile>>? = null
+            var query: PropFindMethod? = null
+            val url = "${client.baseUri}/remote.php/dav/photos/${client.userId}/albums${
+                WebdavUtils.encodePath(
+                    mRemotePath
                 )
-            } else {
+            }"
+            try {
+                // remote request
+                query =
+                    PropFindMethod(
+                        url,
+                        WebdavUtils.getAllPropSet(), // PropFind Properties
+                        DavConstants.DEPTH_1
+                    )
+                val status =
+                    client.executeMethod(
+                        query,
+                        sessionTimeOut.readTimeOut,
+                        sessionTimeOut.connectionTimeOut
+                    )
+
+                // check and process response
+                val isSuccess = (status == HttpStatus.SC_MULTI_STATUS || status == HttpStatus.SC_OK)
+
+                result =
+                    if (isSuccess) {
+                        // get data from remote folder
+                        val dataInServer = query.responseBodyAsMultiStatus
+                        val mFolderAndFiles = WebDavFileUtils().readAlbumData(dataInServer, client)
+
+                        // Result of the operation
+                        RemoteOperationResult<List<RemoteFile>>(true, query).apply {
+                            // Add data to the result
+                            resultData = mFolderAndFiles
+                        }
+                    } else {
+                        // synchronization failed
+                        client.exhaustResponse(query.responseBodyAsStream)
+                        RemoteOperationResult(false, query)
+                    }
+            } catch (e: Exception) {
+                result = RemoteOperationResult(e)
+            } finally {
+                query?.releaseConnection()
+
+                result = result ?: RemoteOperationResult<List<RemoteFile>>(Exception("unknown error")).also {
+                    Log_OC.e(TAG, "Synchronized $mRemotePath: failed")
+                }
                 if (result.isSuccess) {
                     Log_OC.i(TAG, "Synchronized $mRemotePath : ${result.logMessage}")
+                } else if (result.isException) {
+                    Log_OC.e(TAG, "Synchronized $mRemotePath : ${result.logMessage}", result.exception)
                 } else {
-                    if (result.isException) {
-                        Log_OC.e(
-                            TAG, "Synchronized $mRemotePath : ${result.logMessage}",
-                            result.exception
-                        )
-                    } else {
-                        Log_OC.e(TAG, "Synchronized $mRemotePath : ${result.logMessage}")
-                    }
+                    Log_OC.e(TAG, "Synchronized $mRemotePath : ${result.logMessage}")
                 }
             }
+
+            return result
         }
 
-        return result
+        companion object {
+            private val TAG: String = ReadAlbumItemsRemoteOperation::class.java.simpleName
+        }
     }
-
-    companion object {
-        private val TAG: String = ReadAlbumItemsRemoteOperation::class.java.simpleName
-    }
-}
--- library/src/main/java/com/owncloud/android/lib/resources/albums/ReadAlbumsRemoteOperation.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/albums/ReadAlbumsRemoteOperation.kt
@@ -18,53 +18,57 @@ import org.apache.commons.httpclient.HttpStatus
 import org.apache.jackrabbit.webdav.DavConstants
 import org.apache.jackrabbit.webdav.client.methods.PropFindMethod
 
-class ReadAlbumsRemoteOperation @JvmOverloads constructor(
-    private val mAlbumRemotePath: String? = null,
-    private val sessionTimeOut: SessionTimeOut = defaultSessionTimeOut
-) :
-    RemoteOperation<List<PhotoAlbumEntry>>() {
-    /**
-     * Performs the operation.
-     *
-     * @param client Client object to communicate with the remote ownCloud server.
-     */
-    @Deprecated("Deprecated in Java")
-    override fun run(client: OwnCloudClient): RemoteOperationResult<List<PhotoAlbumEntry>> {
-        var propfind: PropFindMethod? = null
-        var result: RemoteOperationResult<List<PhotoAlbumEntry>>
-        var url = "${client.baseUri}/remote.php/dav/photos/${client.userId}/albums"
-        if (!TextUtils.isEmpty(mAlbumRemotePath)) {
-            url += WebdavUtils.encodePath(mAlbumRemotePath)
-        }
-        try {
-            propfind = PropFindMethod(url, WebdavUtils.getAlbumPropSet(), DavConstants.DEPTH_1)
-            val status = client.executeMethod(
-                propfind,
-                sessionTimeOut.readTimeOut,
-                sessionTimeOut.connectionTimeOut
-            )
-            val isSuccess = status == HttpStatus.SC_MULTI_STATUS || status == HttpStatus.SC_OK
-            if (isSuccess) {
-                val albumsList = propfind.responseBodyAsMultiStatus.responses
-                    .filter { it.status[0].statusCode == HttpStatus.SC_OK }
-                    .map { res -> PhotoAlbumEntry(res) }
-                result = RemoteOperationResult<List<PhotoAlbumEntry>>(true, propfind)
-                result.resultData = albumsList
-            } else {
-                result = RemoteOperationResult<List<PhotoAlbumEntry>>(false, propfind)
-                client.exhaustResponse(propfind.responseBodyAsStream)
+class ReadAlbumsRemoteOperation
+    @JvmOverloads
+    constructor(
+        private val mAlbumRemotePath: String? = null,
+        private val sessionTimeOut: SessionTimeOut = defaultSessionTimeOut
+    ) : RemoteOperation<List<PhotoAlbumEntry>>() {
+        /**
+         * Performs the operation.
+         *
+         * @param client Client object to communicate with the remote ownCloud server.
+         */
+        @Deprecated("Deprecated in Java")
+        @Suppress("TooGenericExceptionCaught")
+        override fun run(client: OwnCloudClient): RemoteOperationResult<List<PhotoAlbumEntry>> {
+            var propfind: PropFindMethod? = null
+            var result: RemoteOperationResult<List<PhotoAlbumEntry>>
+            var url = "${client.baseUri}/remote.php/dav/photos/${client.userId}/albums"
+            if (!TextUtils.isEmpty(mAlbumRemotePath)) {
+                url += WebdavUtils.encodePath(mAlbumRemotePath)
             }
-        } catch (e: Exception) {
-            result = RemoteOperationResult<List<PhotoAlbumEntry>>(e)
-            Log_OC.e(TAG, "Read album failed: ${result.logMessage}", result.exception)
-        } finally {
-            propfind?.releaseConnection()
+            try {
+                propfind = PropFindMethod(url, WebdavUtils.getAlbumPropSet(), DavConstants.DEPTH_1)
+                val status =
+                    client.executeMethod(
+                        propfind,
+                        sessionTimeOut.readTimeOut,
+                        sessionTimeOut.connectionTimeOut
+                    )
+                val isSuccess = status == HttpStatus.SC_MULTI_STATUS || status == HttpStatus.SC_OK
+                if (isSuccess) {
+                    val albumsList =
+                        propfind.responseBodyAsMultiStatus.responses
+                            .filter { it.status[0].statusCode == HttpStatus.SC_OK }
+                            .map { res -> PhotoAlbumEntry(res) }
+                    result = RemoteOperationResult<List<PhotoAlbumEntry>>(true, propfind)
+                    result.resultData = albumsList
+                } else {
+                    result = RemoteOperationResult<List<PhotoAlbumEntry>>(false, propfind)
+                    client.exhaustResponse(propfind.responseBodyAsStream)
+                }
+            } catch (e: Exception) {
+                result = RemoteOperationResult<List<PhotoAlbumEntry>>(e)
+                Log_OC.e(TAG, "Read album failed: ${result.logMessage}", result.exception)
+            } finally {
+                propfind?.releaseConnection()
+            }
+
+            return result
         }
 
-        return result
+        companion object {
+            private val TAG: String = ReadAlbumsRemoteOperation::class.java.simpleName
+        }
     }
-
-    companion object {
-        private val TAG: String = ReadAlbumsRemoteOperation::class.java.simpleName
-    }
-}
