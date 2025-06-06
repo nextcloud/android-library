69216433d2287f4b6e50915868e139e347bca41f
A117870935 <surinder.kumar@t-systems.com>
2025-06-06 14:12:51 +0530

    Fix
    Signed-off-by: Surinder Kumar <surinder.kumar@t-systems.com>

--- library/src/main/java/com/owncloud/android/lib/resources/albums/RemoveAlbumFileRemoteOperation.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/albums/RemoveAlbumFileRemoteOperation.kt
@@ -17,42 +17,47 @@ import com.owncloud.android.lib.resources.files.RemoveFileRemoteOperation
 import org.apache.commons.httpclient.HttpStatus
 import org.apache.jackrabbit.webdav.client.methods.DeleteMethod
 
-class RemoveAlbumFileRemoteOperation @JvmOverloads constructor(
-    private val mRemotePath: String,
-    private val sessionTimeOut: SessionTimeOut = defaultSessionTimeOut
-) :
-    RemoteOperation<Any>() {
-    @Deprecated("Deprecated in Java")
-    override fun run(client: OwnCloudClient): RemoteOperationResult<Any> {
-        var result: RemoteOperationResult<Any>
-        var delete: DeleteMethod? = null
-        val webDavUrl = "${client.davUri}/photos/"
-        val encodedPath = ("${client.userId}${Uri.encode(this.mRemotePath)}").replace("%2F", "/")
-        val fullFilePath = "$webDavUrl$encodedPath"
+class RemoveAlbumFileRemoteOperation
+    @JvmOverloads
+    constructor(
+        private val mRemotePath: String,
+        private val sessionTimeOut: SessionTimeOut = defaultSessionTimeOut
+    ) : RemoteOperation<Any>() {
+        @Deprecated("Deprecated in Java")
+        @Suppress("TooGenericExceptionCaught")
+        override fun run(client: OwnCloudClient): RemoteOperationResult<Any> {
+            var result: RemoteOperationResult<Any>
+            var delete: DeleteMethod? = null
+            val webDavUrl = "${client.davUri}/photos/"
+            val encodedPath = ("${client.userId}${Uri.encode(this.mRemotePath)}").replace("%2F", "/")
+            val fullFilePath = "$webDavUrl$encodedPath"
 
-        try {
-            delete = DeleteMethod(fullFilePath)
-            val status = client.executeMethod(
-                delete,
-                sessionTimeOut.readTimeOut, sessionTimeOut.connectionTimeOut
-            )
-            delete.responseBodyAsString
-            result = RemoteOperationResult<Any>(
-                delete.succeeded() || status == HttpStatus.SC_NOT_FOUND,
-                delete
-            )
-            Log_OC.i(TAG, "Remove ${this.mRemotePath} : ${result.logMessage}")
-        } catch (e: Exception) {
-            result = RemoteOperationResult<Any>(e)
-            Log_OC.e(TAG, "Remove ${this.mRemotePath} : ${result.logMessage}", e)
-        } finally {
-            delete?.releaseConnection()
+            try {
+                delete = DeleteMethod(fullFilePath)
+                val status =
+                    client.executeMethod(
+                        delete,
+                        sessionTimeOut.readTimeOut,
+                        sessionTimeOut.connectionTimeOut
+                    )
+                delete.responseBodyAsString
+                result =
+                    RemoteOperationResult<Any>(
+                        delete.succeeded() || status == HttpStatus.SC_NOT_FOUND,
+                        delete
+                    )
+                Log_OC.i(TAG, "Remove ${this.mRemotePath} : ${result.logMessage}")
+            } catch (e: Exception) {
+                result = RemoteOperationResult<Any>(e)
+                Log_OC.e(TAG, "Remove ${this.mRemotePath} : ${result.logMessage}", e)
+            } finally {
+                delete?.releaseConnection()
+            }
+
+            return result
         }
 
-        return result
+        companion object {
+            private val TAG: String = RemoveFileRemoteOperation::class.java.simpleName
+        }
     }
-
-    companion object {
-        private val TAG: String = RemoveFileRemoteOperation::class.java.simpleName
-    }
-}
