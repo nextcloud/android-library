127a8c3340294e91a0e4d648e6cbd3fa96113555
Surinder Kumar <surinder.kumar@t-systems.com>
2025-06-19 20:15:03 +0530

    Fix Copyright
    Signed-off-by: Surinder Kumar <surinder.kumar@t-systems.com>

--- library/src/main/java/com/owncloud/android/lib/resources/albums/CreateNewAlbumRemoteOperation.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/albums/CreateNewAlbumRemoteOperation.kt
@@ -2,7 +2,7 @@
  * Nextcloud Android Library
  *
  * SPDX-FileCopyrightText: 2025 TSI-mc <surinder.kumar@t-systems.com>
- * SPDX-License-Identifier: AGPL-3.0-or-later
+ * SPDX-License-Identifier: MIT
  */
 package com.owncloud.android.lib.resources.albums
 
