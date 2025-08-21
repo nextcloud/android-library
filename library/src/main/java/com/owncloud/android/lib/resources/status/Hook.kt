3f62a46b0b378bfed0a9b0e34dd5ca907c839157
tobiasKaminsky <tobias@kaminsky.me>
2025-08-21 08:52:52 +0200

    wip
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- library/src/main/java/com/owncloud/android/lib/resources/status/Hook.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/status/Hook.kt
@@ -7,6 +7,15 @@
 
 package com.owncloud.android.lib.resources.status
 
+import com.google.gson.annotations.SerializedName
 import com.owncloud.android.lib.resources.declarativeui.Endpoint
 
-data class Hook(val endpoints: List<Endpoint>)
+data class Hook(val type: Type, val endpoints: List<Endpoint>)
+
+enum class Type {
+    @SerializedName("context-menu")
+    CONTEXT_MENU,
+
+    @SerializedName("create-new")
+    CREATE_NEW
+}
