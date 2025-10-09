546ee32d02856af1ff62507ac5a2c21dee974715
tobiasKaminsky <tobias@kaminsky.me>
2025-10-09 14:45:20 +0200

    wip
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

---
+++ library/src/main/java/com/nextcloud/android/lib/resources/users/GenerateOneTimeAppPasswordRemoteOperation.java
@@ -0,0 +1,66 @@
+/*
+ * Nextcloud Android Library
+ *
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2025 Tobias Kaminsky
+ * SPDX-License-Identifier: MIT
+ */
+package com.nextcloud.android.lib.resources.users;
+
+
+import com.nextcloud.common.NextcloudClient;
+import com.nextcloud.operations.GetMethod;
+import com.owncloud.android.lib.common.operations.RemoteOperationResult;
+import com.owncloud.android.lib.common.utils.Log_OC;
+import com.owncloud.android.lib.resources.OCSRemoteOperation;
+
+import org.apache.commons.httpclient.HttpStatus;
+import org.json.JSONObject;
+
+/**
+ * Generate an app password via username / login and **onetime** password. Available since Nextcloud 33
+ */
+
+
+public class GenerateOneTimeAppPasswordRemoteOperation extends OCSRemoteOperation<String> {
+    private static final String TAG = GenerateOneTimeAppPasswordRemoteOperation.class.getSimpleName();
+    private static final String DIRECT_ENDPOINT = "/ocs/v2.php/core/getapppassword-onetime";
+
+    // JSON node names
+    private static final String NODE_OCS = "ocs";
+    private static final String NODE_DATA = "data";
+    private static final String NODE_APPPASSWORD = "apppassword";
+    
+    public RemoteOperationResult<String> run(NextcloudClient client) {
+        RemoteOperationResult<String> result;
+        GetMethod getMethod = null;
+
+        try {
+            getMethod = new GetMethod(client.getBaseUri() + DIRECT_ENDPOINT + JSON_FORMAT, true);
+
+            // remote request
+            int status = client.execute(getMethod);
+
+            if (status == HttpStatus.SC_OK) {
+                String response = getMethod.getResponseBodyAsString();
+
+                JSONObject respJSON = new JSONObject(response);
+                String password = respJSON.getJSONObject(NODE_OCS).getJSONObject(NODE_DATA).getString(NODE_APPPASSWORD);
+
+                result = new RemoteOperationResult<>(true, getMethod);
+                result.setResultData(password);
+            } else {
+                result = new RemoteOperationResult<>(false, getMethod);
+            }
+        } catch (Exception e) {
+            result = new RemoteOperationResult<>(e);
+            Log_OC.e(TAG, "Generate app password failed: " + result.getLogMessage(),
+                    result.getException());
+        } finally {
+            if (getMethod != null) {
+                getMethod.releaseConnection();
+            }
+        }
+        return result;
+    }
+}
