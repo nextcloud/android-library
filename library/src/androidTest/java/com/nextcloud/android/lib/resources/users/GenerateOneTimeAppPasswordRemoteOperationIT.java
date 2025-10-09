3c662b373df51ccc935cfca24ab78fdd3f3f8675
tobiasKaminsky <tobias@kaminsky.me>
2025-10-09 14:45:20 +0200

    wip
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

---
+++ library/src/androidTest/java/com/nextcloud/android/lib/resources/users/GenerateOneTimeAppPasswordRemoteOperationIT.java
@@ -0,0 +1,43 @@
+/*
+ * Nextcloud Android Library
+ *
+ * SPDX-FileCopyrightText: 2020-2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2020 Tobias Kaminsky <tobias@kaminsky.me>
+ * SPDX-License-Identifier: MIT
+ */
+package com.nextcloud.android.lib.resources.users;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+import android.net.Uri;
+import android.text.TextUtils;
+
+import com.owncloud.android.AbstractIT;
+import com.owncloud.android.lib.common.operations.RemoteOperationResult;
+
+import org.junit.Test;
+
+import okhttp3.Credentials;
+
+public class GenerateOneTimeAppPasswordRemoteOperationIT extends AbstractIT {
+
+    @Test
+    public void generateAppPassword() {
+        // nc://onetime-login/user:user1&password:Z8i8J-QLDbr-mSn9A-ijXzN-NSBSt&server:https://qr.ltd3.nextcloud.com
+
+        nextcloudClient.setBaseUri(Uri.parse("https://qr.ltd3.nextcloud.com"));
+        nextcloudClient.setCredentials(Credentials.basic("user1", "9swi2-c9WGn-29cPg-eRNJm-XknFJ"));
+
+        GenerateOneTimeAppPasswordRemoteOperation sut = new GenerateOneTimeAppPasswordRemoteOperation();
+        RemoteOperationResult<String> result = sut.execute(nextcloudClient);
+
+        assertTrue(result.isSuccess());
+
+        String appPassword = result.getResultData();
+        assertFalse(TextUtils.isEmpty(appPassword));
+
+        // re-using onetime password should fail
+        assertFalse(new GenerateOneTimeAppPasswordRemoteOperation().execute(nextcloudClient).isSuccess());
+    }
+}
