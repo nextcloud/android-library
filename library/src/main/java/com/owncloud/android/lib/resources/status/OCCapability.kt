c8d2503d34f51d626d6cedc0dbcc4e2f7044bdf2
tobiasKaminsky <tobias@kaminsky.me>
2025-10-10 07:58:16 +0200

    store subscription info
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- library/src/main/java/com/owncloud/android/lib/resources/status/OCCapability.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/status/OCCapability.kt
@@ -126,6 +126,9 @@ class OCCapability {
 
     var defaultPermissions: Int? = null
 
+    // support
+    var hasValidSubscription = CapabilityBooleanType.UNKNOWN
+
     val version: OwnCloudVersion
         get() =
             OwnCloudVersion(
