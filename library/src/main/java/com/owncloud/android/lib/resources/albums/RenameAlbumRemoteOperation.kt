8d492518fdc1dad42b1b5272359be1c1adfacf51
Surinder Kumar <surinder.kumar@t-systems.com>
2026-01-22 21:08:36 +0530

    minor albums api fixes
    Signed-off-by: Surinder Kumar <surinder.kumar@t-systems.com>

--- library/src/main/java/com/owncloud/android/lib/resources/albums/RenameAlbumRemoteOperation.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/albums/RenameAlbumRemoteOperation.kt
@@ -1,7 +1,7 @@
 /*
  * Nextcloud Android Library
  *
- * SPDX-FileCopyrightText: 2025 TSI-mc <surinder.kumar@t-systems.com>
+ * SPDX-FileCopyrightText: 2026 TSI-mc <surinder.kumar@t-systems.com>
  * SPDX-License-Identifier: MIT
  */
 package com.owncloud.android.lib.resources.albums
@@ -13,65 +13,72 @@ import com.owncloud.android.lib.common.network.WebdavUtils
 import com.owncloud.android.lib.common.operations.RemoteOperation
 import com.owncloud.android.lib.common.operations.RemoteOperationResult
 import com.owncloud.android.lib.common.utils.Log_OC
+import org.apache.commons.httpclient.HttpStatus
 import org.apache.jackrabbit.webdav.client.methods.MoveMethod
 
 class RenameAlbumRemoteOperation
-    @JvmOverloads
-    constructor(
-        private val mOldRemotePath: String,
-        val newAlbumName: String,
-        private val sessionTimeOut: SessionTimeOut = defaultSessionTimeOut
-    ) : RemoteOperation<Any>() {
-        /**
-         * Performs the operation.
-         *
-         * @param client Client object to communicate with the remote ownCloud server.
-         */
-        @Deprecated("Deprecated in Java")
-        @Suppress("TooGenericExceptionCaught")
-        override fun run(client: OwnCloudClient): RemoteOperationResult<Any>? {
-            var result: RemoteOperationResult<Any>? = null
-            var move: MoveMethod? = null
-            val url = "${client.baseUri}/remote.php/dav/photos/${client.userId}/albums"
-            try {
-                if (this.newAlbumName != this.mOldRemotePath) {
-                    move =
-                        MoveMethod(
-                            "$url${WebdavUtils.encodePath(mOldRemotePath)}",
-                            "$url${
-                                WebdavUtils.encodePath(
-                                    newAlbumName
-                                )
-                            }",
-                            true
+@JvmOverloads
+constructor(
+    private val mOldRemotePath: String,
+    val newAlbumName: String,
+    private val sessionTimeOut: SessionTimeOut = defaultSessionTimeOut
+) : RemoteOperation<Any>() {
+    /**
+     * Performs the operation.
+     *
+     * @param client Client object to communicate with the remote ownCloud server.
+     */
+    @Deprecated("Deprecated in Java")
+    @Suppress("TooGenericExceptionCaught")
+    override fun run(client: OwnCloudClient): RemoteOperationResult<Any>? {
+        if (this.newAlbumName == this.mOldRemotePath) {
+            return RemoteOperationResult(RemoteOperationResult.ResultCode.OK)
+        }
+
+        var result: RemoteOperationResult<Any>? = null
+        var move: MoveMethod? = null
+        val url = "${client.baseUri}/remote.php/dav/photos/${client.userId}/albums"
+        try {
+            move =
+                MoveMethod(
+                    "$url${WebdavUtils.encodePath(mOldRemotePath)}",
+                    "$url${
+                        WebdavUtils.encodePath(
+                            newAlbumName
                         )
-                    client.executeMethod(
-                        move,
-                        sessionTimeOut.readTimeOut,
-                        sessionTimeOut.connectionTimeOut
-                    )
-                    result = RemoteOperationResult<Any>(move.succeeded(), move)
-                    Log_OC.i(
-                        TAG,
-                        "Rename ${this.mOldRemotePath} to ${this.newAlbumName} : ${result.logMessage}"
-                    )
-                    client.exhaustResponse(move.responseBodyAsStream)
-                }
-            } catch (e: Exception) {
-                result = RemoteOperationResult<Any>(e)
-                Log_OC.e(
-                    TAG,
-                    "Rename ${this.mOldRemotePath} to ${this.newAlbumName} : ${result.logMessage}",
-                    e
+                    }",
+                    false
                 )
-            } finally {
-                move?.releaseConnection()
+            client.executeMethod(
+                move,
+                sessionTimeOut.readTimeOut,
+                sessionTimeOut.connectionTimeOut
+            )
+            result = RemoteOperationResult<Any>(move.succeeded(), move)
+            Log_OC.i(
+                TAG,
+                "Rename ${this.mOldRemotePath} to ${this.newAlbumName} : ${result.logMessage}"
+            )
+            // album name already exist
+            if (!result.isSuccess && result.httpCode == HttpStatus.SC_PRECONDITION_FAILED) {
+                result = RemoteOperationResult<Any>(RemoteOperationResult.ResultCode.INVALID_OVERWRITE)
             }
-
-            return result
+            client.exhaustResponse(move.responseBodyAsStream)
+        } catch (e: Exception) {
+            result = RemoteOperationResult<Any>(e)
+            Log_OC.e(
+                TAG,
+                "Rename ${this.mOldRemotePath} to ${this.newAlbumName} : ${result.logMessage}",
+                e
+            )
+        } finally {
+            move?.releaseConnection()
         }
 
-        companion object {
-            private val TAG: String = RenameAlbumRemoteOperation::class.java.simpleName
-        }
+        return result
     }
+
+    companion object {
+        private val TAG: String = RenameAlbumRemoteOperation::class.java.simpleName
+    }
+}
