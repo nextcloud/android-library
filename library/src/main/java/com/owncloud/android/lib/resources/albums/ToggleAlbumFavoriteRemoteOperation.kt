890ae2fc8d0e3faefd5e4e472c1a44310da7323f
Surinder Kumar <surinder.kumar@t-systems.com>
2026-01-22 21:08:36 +0530

    minor albums api fixes
    Signed-off-by: Surinder Kumar <surinder.kumar@t-systems.com>

--- library/src/main/java/com/owncloud/android/lib/resources/albums/ToggleAlbumFavoriteRemoteOperation.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/albums/ToggleAlbumFavoriteRemoteOperation.kt
@@ -1,18 +1,19 @@
 /*
  * Nextcloud Android Library
  *
- * SPDX-FileCopyrightText: 2025 TSI-mc <surinder.kumar@t-systems.com>
+ * SPDX-FileCopyrightText: 2026 TSI-mc <surinder.kumar@t-systems.com>
  * SPDX-License-Identifier: MIT
  */
 package com.owncloud.android.lib.resources.albums
 
-import android.net.Uri
 import com.nextcloud.common.SessionTimeOut
 import com.nextcloud.common.defaultSessionTimeOut
 import com.owncloud.android.lib.common.OwnCloudClient
 import com.owncloud.android.lib.common.network.WebdavEntry
+import com.owncloud.android.lib.common.network.WebdavUtils
 import com.owncloud.android.lib.common.operations.RemoteOperation
 import com.owncloud.android.lib.common.operations.RemoteOperationResult
+import com.owncloud.android.lib.resources.files.ToggleFavoriteRemoteOperation
 import org.apache.commons.httpclient.HttpStatus
 import org.apache.jackrabbit.webdav.client.methods.PropPatchMethod
 import org.apache.jackrabbit.webdav.property.DavPropertyNameSet
@@ -24,30 +25,37 @@ import java.io.IOException
 class ToggleAlbumFavoriteRemoteOperation
     @JvmOverloads
     constructor(
-        private val makeItFavorited: Boolean,
+        private val markAsFavorite: Boolean,
         private val filePath: String,
         private val sessionTimeOut: SessionTimeOut = defaultSessionTimeOut
     ) : RemoteOperation<Any>() {
         @Deprecated("Deprecated in Java")
+        @Suppress("ReturnCount")
         override fun run(client: OwnCloudClient): RemoteOperationResult<Any> {
+            if (filePath.isEmpty() || filePath.isBlank()) {
+                return RemoteOperationResult(RemoteOperationResult.ResultCode.OK)
+            }
+
+            // when file is in local db the remotePath will be actual path instead of albums path
+            // to perform operation we have to call files dav uri
+            if (!filePath.contains("/albums/")) {
+                return ToggleFavoriteRemoteOperation(markAsFavorite, filePath).execute(client)
+            }
+
             var result: RemoteOperationResult<Any>
             var propPatchMethod: PropPatchMethod? = null
             val newProps = DavPropertySet()
             val removeProperties = DavPropertyNameSet()
-            if (this.makeItFavorited) {
                 val favoriteProperty =
                     DefaultDavProperty<Any>(
                         "oc:favorite",
-                        "1",
+                        if(markAsFavorite) "1" else "0",
                         Namespace.getNamespace(WebdavEntry.NAMESPACE_OC)
                     )
                 newProps.add(favoriteProperty)
-            } else {
-                removeProperties.add("oc:favorite", Namespace.getNamespace(WebdavEntry.NAMESPACE_OC))
-            }
 
             val webDavUrl = "${client.davUri}/photos/"
-            val encodedPath = ("${client.userId}${Uri.encode(this.filePath)}").replace("%2F", "/")
+            val encodedPath = "${client.userId}${WebdavUtils.encodePath(this.filePath)}"
             val fullFilePath = "$webDavUrl$encodedPath"
 
             try {
