5082b9c62c0b851e14352a64cbc9f36eed45a78c
tobiasKaminsky <tobias@kaminsky.me>
2026-02-05 10:55:30 +0100

    wip
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- library/src/androidTest/java/com/nextcloud/android/lib/resources/recommendations/GetRecommendationsRemoteOperationIT.kt
+++ library/src/androidTest/java/com/nextcloud/android/lib/resources/recommendations/GetRecommendationsRemoteOperationIT.kt
@@ -19,12 +19,8 @@ class GetRecommendationsRemoteOperationIT : AbstractIT() {
         testOnlyOnServer(NextcloudVersion.nextcloud_31)
         assertTrue(CreateFolderRemoteOperation("/test/", true).execute(client).isSuccess)
 
-        val sut = GetRecommendationsRemoteOperation().execute(nextcloudClient)
-        
-        assertTrue(sut.exception.message, sut.isSuccess)
+        val result = GetRecommendationsRemoteOperation().execute(nextcloudClient).resultData
 
-        val result = sut.resultData
-        
         assertTrue(result.enabled)
         assertTrue(result.recommendations.isNotEmpty())
     }
