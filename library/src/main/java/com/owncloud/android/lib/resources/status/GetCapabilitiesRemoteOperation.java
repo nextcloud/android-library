0428a73f4f91e43dcceb0c65ff1aba6a1ba400fd
alperozturk96 <alper_ozturk@proton.me>
2026-05-29 14:46:34 +0300

    feat(unified-share): new capabilities
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- library/src/main/java/com/owncloud/android/lib/resources/status/GetCapabilitiesRemoteOperation.java
+++ library/src/main/java/com/owncloud/android/lib/resources/status/GetCapabilitiesRemoteOperation.java
@@ -169,7 +169,10 @@ public class GetCapabilitiesRemoteOperation extends RemoteOperation<OCCapability
 
     //recommendations
     private static final String NODE_RECOMMENDATIONS = "recommendations";
-    
+
+    // sharing
+    private static final String NODE_SHARING = "sharing";
+
     // needed for checking compatible filenames
     private static final String FORBIDDEN_FILENAME_CHARACTERS = "forbidden_filename_characters";
     private static final String FORBIDDEN_FILENAMES = "forbidden_filenames";
@@ -827,6 +830,11 @@ public class GetCapabilitiesRemoteOperation extends RemoteOperation<OCCapability
                     }
                 } else {
                     capability.setRecommendations(CapabilityBooleanType.FALSE);
+                }
+
+                if (respCapabilities.has(NODE_SHARING)) {
+                    JSONObject sharingCapability = respCapabilities.getJSONObject(NODE_SHARING);
+                    capability.setSharingJson(sharingCapability.toString());
                 }
 
                 // notes folder
