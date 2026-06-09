7c5d7e970f3a0be12d84897ad16771a3d3e6c2e5
alperozturk96 <alper_ozturk@proton.me>
2026-06-09 09:54:40 +0200

    convert to kotlin
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- library/src/main/java/com/owncloud/android/lib/resources/files/DownloadFileRemoteOperation.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/files/DownloadFileRemoteOperation.kt
@@ -1,185 +1,162 @@
 /*
  * Nextcloud Android Library
  *
- * SPDX-FileCopyrightText: 2015 ownCloud Inc.
+ * SPDX-FileCopyrightText: 2026 Alper Ozturk <alper.ozturk@nextcloud.com>
  * SPDX-License-Identifier: MIT
  */
-package com.owncloud.android.lib.resources.files;
+package com.owncloud.android.lib.resources.files
 
-import com.nextcloud.common.NextcloudClient;
-import com.nextcloud.operations.GetMethod;
-import com.owncloud.android.lib.common.network.OnDatatransferProgressListener;
-import com.owncloud.android.lib.common.network.WebdavUtils;
-import com.owncloud.android.lib.common.operations.OperationCancelledException;
-import com.owncloud.android.lib.common.operations.RemoteOperation;
-import com.owncloud.android.lib.common.operations.RemoteOperationResult;
-import com.owncloud.android.lib.common.utils.Log_OC;
+import com.nextcloud.common.NextcloudClient
+import com.nextcloud.operations.GetMethod
+import com.owncloud.android.lib.common.network.OnDatatransferProgressListener
+import com.owncloud.android.lib.common.network.WebdavUtils
+import com.owncloud.android.lib.common.operations.OperationCancelledException
+import com.owncloud.android.lib.common.operations.RemoteOperation
+import com.owncloud.android.lib.common.operations.RemoteOperationResult
+import com.owncloud.android.lib.common.utils.Log_OC
+import org.apache.commons.httpclient.HttpStatus
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.concurrent.atomic.AtomicBoolean
 
-import org.apache.commons.httpclient.HttpStatus;
+class DownloadFileRemoteOperation(private val remotePath: String, private val temporalFolderPath: String?) :
+    RemoteOperation<Any?>() {
+    private val dataTransferListeners: MutableSet<OnDatatransferProgressListener?> =
+        HashSet()
+    private val cancellationRequested = AtomicBoolean(false)
+    var modificationTimestamp: Long = 0
+        private set
+    var etag: String = ""
+        private set
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-/**
- * Remote operation performing the download of a remote file in the ownCloud server.
- *
- * @author David A. Velasco
- * @author masensio
- */
-
-public class DownloadFileRemoteOperation extends RemoteOperation {
-
-    private static final String TAG = DownloadFileRemoteOperation.class.getSimpleName();
-
-    private final Set<OnDatatransferProgressListener> mDataTransferListeners = new HashSet<>();
-    private final AtomicBoolean mCancellationRequested = new AtomicBoolean(false);
-    private long modificationTimestamp = 0;
-    private String eTag = "";
-    private GetMethod getMethod;
-
-    private final String remotePath;
-    private final String temporalFolderPath;
-
-    /**
-     * @param remotePath         which file to download
-     * @param temporalFolderPath temporal folder where file is stored, to avoid conflicts it use full remote path
-     */
-    public DownloadFileRemoteOperation(String remotePath, String temporalFolderPath) {
-        this.remotePath = remotePath;
-        this.temporalFolderPath = temporalFolderPath;
-    }
-
-	@Override
-	public RemoteOperationResult run(NextcloudClient client) {
-        RemoteOperationResult<?> result;
-        File tmpFile = new File(getTmpPath());
+    override fun run(client: NextcloudClient): RemoteOperationResult<Any> {
+        var result: RemoteOperationResult<Any>
+        val tmpFile = File(this.tmpPath)
 
         try {
-            final var parentFile = tmpFile.getParentFile();
-            if (parentFile == null) {
-                return new RemoteOperationResult<>(new Exception("parent file is null"));
-            }
+            val parentFile =
+                tmpFile.getParentFile() ?: return RemoteOperationResult<Any>(Exception("parent file is null"))
 
-            boolean isTempFileCreated = parentFile.mkdirs();
+            val isTempFileCreated = parentFile.mkdirs()
             if (isTempFileCreated) {
-                Log_OC.d(TAG, "temp file created");
+                Log_OC.d(TAG, "temp file created")
             }
 
-            int status = downloadFile(client, tmpFile);
-            result = new RemoteOperationResult<>(isSuccess(status), getMethod);
-            Log_OC.i(TAG, "Download of " + remotePath + " to " + getTmpPath() + ": " +
-                result.getLogMessage());
-
-        } catch (Exception e) {
-            result = new RemoteOperationResult<>(e);
-            Log_OC.e(TAG, "Download of " + remotePath + " to " + getTmpPath() + ": " +
-                result.getLogMessage(), e);
+            val getMethod = GetMethod(client.getFilesDavUri(remotePath), false)
+            val status = downloadFile(getMethod, client, tmpFile)
+            result = RemoteOperationResult<Any>(isSuccess(status), getMethod)
+            Log_OC.i(
+                TAG, "Download of " + remotePath + " to " + this.tmpPath + ": " +
+                    result.getLogMessage()
+            )
+        } catch (e: Exception) {
+            result = RemoteOperationResult<Any>(e)
+            Log_OC.e(
+                TAG, "Download of " + remotePath + " to " + this.tmpPath + ": " +
+                    result.getLogMessage(), e
+            )
         }
 
-        return result;
+        return result
     }
 
+    @Throws(IOException::class, OperationCancelledException::class, CreateLocalFileException::class)
+    private fun downloadFile(getMethod: GetMethod, client: NextcloudClient, targetFile: File): Int {
+        var status: Int
+        var it: MutableIterator<OnDatatransferProgressListener?>?
 
-    private int downloadFile(NextcloudClient client, File targetFile) throws IOException, OperationCancelledException, CreateLocalFileException {
-        int status;
-        getMethod = new GetMethod(client.getFilesDavUri(remotePath), false);
-        Iterator<OnDatatransferProgressListener> it;
-
-        FileOutputStream fos = null;
+        var fos: FileOutputStream? = null
         try {
-            status = client.execute(getMethod);
+            status = client.execute(getMethod)
 
             if (isSuccess(status)) {
                 try {
-                    boolean isTargetFileCreated = targetFile.createNewFile();
+                    val isTargetFileCreated = targetFile.createNewFile()
                     if (isTargetFileCreated) {
-                        Log_OC.i(TAG, "target file is created");
+                        Log_OC.i(TAG, "target file is created")
                     }
-                } catch (IOException | SecurityException ex) {
-                    Log_OC.e(TAG, "Error creating file " + targetFile.getAbsolutePath(), ex);
-                    throw new CreateLocalFileException(targetFile.getPath(), ex);
+                } catch (ex: IOException) {
+                    Log_OC.e(TAG, "Error creating file " + targetFile.absolutePath, ex)
+                    throw CreateLocalFileException(targetFile.path, ex)
+                } catch (ex: SecurityException) {
+                    Log_OC.e(TAG, "Error creating file " + targetFile.absolutePath, ex)
+                    throw CreateLocalFileException(targetFile.path, ex)
                 }
 
-                BufferedInputStream bis = new BufferedInputStream(getMethod.getResponseBodyAsStream());
-                fos = new FileOutputStream(targetFile);
+                val bis = BufferedInputStream(getMethod?.getResponseBodyAsStream())
+                fos = FileOutputStream(targetFile)
 
-                byte[] bytes = new byte[4096];
-                int readResult;
-                while ((readResult = bis.read(bytes)) != -1) {
-                    synchronized (mCancellationRequested) {
-                        if (mCancellationRequested.get()) {
-                            throw new OperationCancelledException();
+                val bytes = ByteArray(4096)
+                var readResult: Int
+                while ((bis.read(bytes).also { readResult = it }) != -1) {
+                    synchronized(cancellationRequested) {
+                        if (cancellationRequested.get()) {
+                            throw OperationCancelledException()
                         }
                     }
 
-                    fos.write(bytes, 0, readResult);
+                    fos.write(bytes, 0, readResult)
 
-                    synchronized (mDataTransferListeners) {
-                        it = mDataTransferListeners.iterator();
+                    synchronized(dataTransferListeners) {
+                        it = dataTransferListeners.iterator()
                     }
                 }
 
-
-                String modificationTime = getMethod.getResponseHeader("Last-Modified");
+                var modificationTime = getMethod.getResponseHeader("Last-Modified")
                 if (modificationTime == null) {
-                    modificationTime = getMethod.getResponseHeader("last-modified");
+                    modificationTime = getMethod.getResponseHeader("last-modified")
                 }
 
                 if (modificationTime != null) {
-                    Date d = WebdavUtils.parseResponseDate(modificationTime);
-                    modificationTimestamp = (d != null) ? d.getTime() : 0;
+                    val d = WebdavUtils.parseResponseDate(modificationTime)
+                    modificationTimestamp = d?.time ?: 0
                 } else {
-                    Log_OC.e(TAG, "Could not read modification time from response downloading " + remotePath);
+                    Log_OC.e(
+                        TAG,
+                        "Could not read modification time from response downloading $remotePath"
+                    )
                 }
 
-                eTag = WebdavUtils.getEtagFromResponse(getMethod);
-                if (eTag.isEmpty()) {
-                    Log_OC.e(TAG, "Could not read eTag from response downloading " + remotePath);
+                this.etag = WebdavUtils.getEtagFromResponse(getMethod)
+                if (etag.isEmpty()) {
+                    Log_OC.e(TAG, "Could not read eTag from response downloading $remotePath")
                 }
             }
         } finally {
-            if (fos != null) fos.close();
-            getMethod.releaseConnection();    // let the connection available for other methods
+            fos?.close()
+
+            // let the connection available for other methods
+            getMethod.releaseConnection()
         }
-        return status;
+        return status
     }
 
-    private boolean isSuccess(int status) {
-        return (status == HttpStatus.SC_OK);
+    private fun isSuccess(status: Int): Boolean {
+        return (status == HttpStatus.SC_OK)
     }
 
-    private String getTmpPath() {
-        return temporalFolderPath + remotePath;
-    }
+    private val tmpPath: String
+        get() = temporalFolderPath + remotePath
 
-    public void addDatatransferProgressListener(OnDatatransferProgressListener listener) {
-        synchronized (mDataTransferListeners) {
-            mDataTransferListeners.add(listener);
-        }
-    }
-
-    public void removeDatatransferProgressListener(OnDatatransferProgressListener listener) {
-        synchronized (mDataTransferListeners) {
-            mDataTransferListeners.remove(listener);
+    fun addDatatransferProgressListener(listener: OnDatatransferProgressListener?) {
+        synchronized(dataTransferListeners) {
+            dataTransferListeners.add(listener)
         }
     }
 
-    public void cancel() {
-        mCancellationRequested.set(true);   // atomic set; there is no need of synchronizing it
+    fun removeDatatransferProgressListener(listener: OnDatatransferProgressListener?) {
+        synchronized(dataTransferListeners) {
+            dataTransferListeners.remove(listener)
+        }
     }
 
-    public long getModificationTimestamp() {
-        return modificationTimestamp;
+    fun cancel() {
+        cancellationRequested.set(true)
     }
 
-    public String getEtag() {
-        return eTag;
+    companion object {
+        private val TAG: String = DownloadFileRemoteOperation::class.java.getSimpleName()
     }
 }
