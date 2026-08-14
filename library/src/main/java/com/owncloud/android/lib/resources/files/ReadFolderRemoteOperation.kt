c56538e26ed84b0133dd4f9347892dff6a6aa5a9
alperozturk96 <alper_ozturk@proton.me>
2026-08-14 13:32:50 +0200

    convert read folder remote operation to kotlin
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- library/src/main/java/com/owncloud/android/lib/resources/files/ReadFolderRemoteOperation.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/files/ReadFolderRemoteOperation.kt
@@ -4,144 +4,100 @@
  * SPDX-FileCopyrightText: 2015 ownCloud Inc.
  * SPDX-License-Identifier: MIT
  */
-package com.owncloud.android.lib.resources.files;
+package com.owncloud.android.lib.resources.files
 
-import com.owncloud.android.lib.common.OwnCloudClient;
-import com.owncloud.android.lib.common.network.WebdavEntry;
-import com.owncloud.android.lib.common.network.WebdavUtils;
-import com.owncloud.android.lib.common.operations.RemoteOperation;
-import com.owncloud.android.lib.common.operations.RemoteOperationResult;
-import com.owncloud.android.lib.common.utils.Log_OC;
-import com.owncloud.android.lib.resources.files.model.RemoteFile;
+import com.owncloud.android.lib.common.OwnCloudClient
+import com.owncloud.android.lib.common.network.WebdavEntry
+import com.owncloud.android.lib.common.network.WebdavUtils
+import com.owncloud.android.lib.common.operations.RemoteOperation
+import com.owncloud.android.lib.common.operations.RemoteOperationResult
+import com.owncloud.android.lib.common.utils.Log_OC
+import com.owncloud.android.lib.resources.files.model.RemoteFile
+import org.apache.commons.httpclient.HttpStatus
+import org.apache.jackrabbit.webdav.DavConstants
+import org.apache.jackrabbit.webdav.MultiStatus
+import org.apache.jackrabbit.webdav.client.methods.PropFindMethod
 
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.jackrabbit.webdav.DavConstants;
-import org.apache.jackrabbit.webdav.MultiStatus;
-import org.apache.jackrabbit.webdav.MultiStatusResponse;
-import org.apache.jackrabbit.webdav.client.methods.PropFindMethod;
+class ReadFolderRemoteOperation(private val remotePath: String?) : RemoteOperation<Any?>() {
+    private var folderAndFiles: ArrayList<Any?>? = null
 
-import java.util.ArrayList;
-
-/**
- * Remote operation performing the read of remote file or folder in the ownCloud server.
- *
- * @author David A. Velasco
- * @author masensio
- */
-
-public class ReadFolderRemoteOperation extends RemoteOperation {
-
-    private static final String TAG = ReadFolderRemoteOperation.class.getSimpleName();
-
-    private String mRemotePath;
-    private ArrayList<Object> mFolderAndFiles;
-
-    /**
-     * Constructor
-     *
-     * @param remotePath Remote path of the file.
-     */
-    public ReadFolderRemoteOperation(String remotePath) {
-        mRemotePath = remotePath;
-    }
-
-    /**
-     * Performs the read operation.
-     *
-     * @param client Client object to communicate with the remote ownCloud server.
-     */
-    @Override
-    protected RemoteOperationResult run(OwnCloudClient client) {
-        RemoteOperationResult<?> result = null;
-        PropFindMethod query = null;
+    override fun run(client: OwnCloudClient): RemoteOperationResult<*> {
+        var result: RemoteOperationResult<*>? = null
+        var query: PropFindMethod? = null
 
         try {
-            // remote request
-            query = new PropFindMethod(client.getFilesDavUri(mRemotePath),
-                    WebdavUtils.getAllPropSet(),    // PropFind Properties
-                    DavConstants.DEPTH_1);
-            int status = client.executeMethod(query);
+            query = PropFindMethod(
+                client.getFilesDavUri(remotePath),
+                WebdavUtils.getAllPropSet(),
+                DavConstants.DEPTH_1
+            )
+            val status = client.executeMethod(query)
 
-            // check and process response
-            boolean isSuccess = (status == HttpStatus.SC_MULTI_STATUS || status == HttpStatus.SC_OK);
-            
+            val isSuccess = (status == HttpStatus.SC_MULTI_STATUS || status == HttpStatus.SC_OK)
+
             if (isSuccess) {
-                // get data from remote folder
-                MultiStatus dataInServer = query.getResponseBodyAsMultiStatus();
-                readData(dataInServer, client);
-
-                // Result of the operation
-                result = new RemoteOperationResult<>(true, query);
-                // Add data to the result
-                if (result.isSuccess()) {
-                    result.setData(mFolderAndFiles);
+                val dataInServer = query.getResponseBodyAsMultiStatus()
+                readData(dataInServer, client)
+                result = RemoteOperationResult<Any?>(true, query)
+                if (result.isSuccess) {
+                    result.data = folderAndFiles
                 }
             } else {
-                // synchronization failed
-                client.exhaustResponse(query.getResponseBodyAsStream());
-                result = new RemoteOperationResult<>(false, query);
+                client.exhaustResponse(query.getResponseBodyAsStream())
+                result = RemoteOperationResult<Any?>(false, query)
             }
-        } catch (OutOfMemoryError e) {
-            mFolderAndFiles = null;
-            Log_OC.e(TAG, "Not enough memory to read the content of " + mRemotePath, e);
-            result = new RemoteOperationResult<>(RemoteOperationResult.ResultCode.OUT_OF_MEMORY);
-        } catch (Exception e) {
-            result = new RemoteOperationResult<>(e);
+        } catch (e: OutOfMemoryError) {
+            folderAndFiles = null
+            Log_OC.e(TAG, "Not enough memory to read the content of $remotePath", e)
+            result = RemoteOperationResult<Any?>(RemoteOperationResult.ResultCode.OUT_OF_MEMORY)
+        } catch (e: Exception) {
+            result = RemoteOperationResult<Any?>(e)
         } finally {
-            if (query != null)
-                query.releaseConnection();  // let the connection available for other methods
+            query?.releaseConnection()
 
             if (result == null) {
-                result = new RemoteOperationResult<>(new Exception("unknown error"));
-                Log_OC.e(TAG, "Synchronized " + mRemotePath + ": failed");
+                result = RemoteOperationResult<Any?>(Exception("unknown error"))
+                Log_OC.e(TAG, "Synchronized $remotePath: failed")
             } else {
-                if (result.isSuccess()) {
-                    Log_OC.i(TAG, "Synchronized " + mRemotePath + ": " + result.getLogMessage());
+                if (result.isSuccess) {
+                    Log_OC.i(TAG, "Synchronized " + remotePath + ": " + result.getLogMessage())
                 } else {
-                    if (result.isException()) {
-                        Log_OC.e(TAG, "Synchronized " + mRemotePath + ": " + result.getLogMessage(),
-                                result.getException());
+                    if (result.isException) {
+                        Log_OC.e(
+                            TAG, "Synchronized " + remotePath + ": " + result.getLogMessage(),
+                            result.exception
+                        )
                     } else {
-                        Log_OC.e(TAG, "Synchronized " + mRemotePath + ": " + result.getLogMessage());
+                        Log_OC.e(TAG, "Synchronized " + remotePath + ": " + result.getLogMessage())
                     }
                 }
             }
         }
-        
-        return result;
+
+        return result
     }
 
-    public boolean isMultiStatus(int status) {
-        return (status == HttpStatus.SC_MULTI_STATUS);
+    fun isMultiStatus(status: Int): Boolean {
+        return (status == HttpStatus.SC_MULTI_STATUS)
     }
 
-    /**
-     * Read the data retrieved from the server about the contents of the target folder
-     *
-     * @param remoteData Full response got from the server with the data of the target
-     *                   folder and its direct children.
-     * @param client     Client instance to the remote server where the data were
-     *                   retrieved.
-     * @return
-     */
-    private void readData(MultiStatus remoteData, OwnCloudClient client) {
-        MultiStatusResponse[] responses = remoteData.getResponses();
-        String davUriPath = client.getFilesDavUri().getEncodedPath();
+    private fun readData(remoteData: MultiStatus, client: OwnCloudClient) {
+        val responses = remoteData.responses
+        val davUriPath = client.filesDavUri.encodedPath
+        folderAndFiles = ArrayList(responses.size)
 
-        mFolderAndFiles = new ArrayList<>(responses.length);
+        var we = WebdavEntry(responses[0]!!, davUriPath!!)
+        folderAndFiles!!.add(RemoteFile(we))
 
-        // parse data from remote folder
-        WebdavEntry we = new WebdavEntry(responses[0], davUriPath);
-        mFolderAndFiles.add(new RemoteFile(we));
-
-        // loop to update every child
-        RemoteFile remoteFile;
-        for (int i = 1; i < responses.length; ++i) {
-            /// new OCFile instance with the data from the server
-            we = new WebdavEntry(responses[i], davUriPath);
-            remoteFile = new RemoteFile(we);
-            mFolderAndFiles.add(remoteFile);
+        var remoteFile: RemoteFile?
+        for (i in 1..<responses.size) {
+            we = WebdavEntry(responses[i]!!, davUriPath)
+            remoteFile = RemoteFile(we)
+            folderAndFiles!!.add(remoteFile)
         }
+    }
 
+    companion object {
+        private val TAG: String = ReadFolderRemoteOperation::class.java.getSimpleName()
     }
 }
